4bf4f7cc9ec5d895bee272ccd0bea6e7e5b55749
Ed Singleton <singletoned@gmail.com>
2022-05-13 10:03:26 +0100

    Add TimeStampedModel

--- ellandi/registration/models.py
+++ ellandi/registration/models.py
@@ -1,3 +1,21 @@
+import datetime
+
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(editable=False, default=datetime.datetime.now)
+    modified_at = models.DateTimeField(editable=False, default=datetime.datetime.now)
+
+    def save(self, *args, **kwargs):
+        update_fields = kwargs.get('update_fields', None)
+        if update_fields:
+            kwargs['update_fields'] = set(update_fields).union({'modified'})
+
+        super().save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
 
 # Create your models here.
