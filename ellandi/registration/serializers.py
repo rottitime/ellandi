f5c91e5c43f85d75a13ce33f5cff1e301798d997
Ed Singleton <singletoned@gmail.com>
2022-05-16 15:44:43 +0100

    Delete group view

--- ellandi/registration/serializers.py
+++ ellandi/registration/serializers.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Group
 from rest_framework import serializers
 
 
@@ -7,9 +6,3 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ["url", "username", "email", "groups"]
-
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ["url", "name"]
--- ellandi/registration/views.py
+++ ellandi/registration/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Group
 from rest_framework import viewsets
 
 from . import serializers
@@ -12,12 +11,3 @@ class UserViewSet(viewsets.ModelViewSet):
 
     queryset = get_user_model().objects.all().order_by("-created_at")
     serializer_class = serializers.UserSerializer
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-
-    queryset = Group.objects.all()
-    serializer_class = serializers.GroupSerializer
