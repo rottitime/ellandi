016fb3a0fb66394942703e06b79fc52cabab65ed
Ed Singleton <singletoned@gmail.com>
2022-05-16 15:43:55 +0100

    Add a couple of views

--- ellandi/registration/views.py
+++ ellandi/registration/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from rest_framework import viewsets
 
-# Create your views here.
+from . import serializers
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+
+    queryset = get_user_model().objects.all().order_by("-created_at")
+    serializer_class = serializers.UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+
+    queryset = Group.objects.all()
+    serializer_class = serializers.GroupSerializer
