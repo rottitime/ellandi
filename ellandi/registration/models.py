c343a87a415c81101eac721dd5755dcc7527ccec
Ed Singleton <singletoned@gmail.com>
2022-05-17 09:52:58 +0100

    Add UserManager

--- ellandi/registration/models.py
+++ ellandi/registration/models.py
@@ -3,6 +3,37 @@ import datetime
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users require an email field')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
 
 class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(editable=False, default=datetime.datetime.now)
@@ -22,6 +53,8 @@ class TimeStampedModel(models.Model):
 class User(AbstractUser, TimeStampedModel):
     username = None
     email = models.EmailField("email", unique=True)
+
+    objects = UserManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
