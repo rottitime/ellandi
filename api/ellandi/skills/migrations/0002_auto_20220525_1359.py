2eadad5e192b00ce13ef7b0d32bbf57287281cb6
oernster <oliverjernster@hotmail.com>
2022-05-25 15:05:53 +0100

    Reblacked

--- api/ellandi/skills/migrations/0002_auto_20220525_1359.py
+++ api/ellandi/skills/migrations/0002_auto_20220525_1359.py
@@ -8,18 +8,18 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('skills', '0001_initial'),
+        ("skills", "0001_initial"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Skill',
+            name="Skill",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('users', models.ManyToManyField(blank=True, related_name='skills', to=settings.AUTH_USER_MODEL)),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("users", models.ManyToManyField(blank=True, related_name="skills", to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.DeleteModel(
-            name='Skills',
+            name="Skills",
         ),
     ]
