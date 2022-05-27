0fb7d44d5d8bb80c196e9876bd5ba0b9f0d4db08
Ed Singleton <singletoned@gmail.com>
2022-05-27 17:43:44 +0100

    Write out env file for web

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -3,4 +3,11 @@
 set -o errexit
 set -o nounset
 
+cat > ../.env <<EOF
+NODE_ENV=production
+VITE_FRONTEND_HOST="$VITE_FRONTEND_HOST"
+VITE_API_ROOT_URL="$VITE_API_ROOT_URL"
+VITE_IS_DEMO_MODE=$VITE_IS_DEMO_MODE
+EOF
+
 npm run prod
