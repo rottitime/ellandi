1221e6437b0680e5b5d25eef534d99b49846f7e4
Ed Singleton <singletoned@gmail.com>
2022-06-01 14:19:10 +0100

    Set DATABASE_URL in start.sh instead

--- docker/api/start.sh
+++ docker/api/start.sh
@@ -3,5 +3,7 @@
 set -o errexit
 set -o nounset
 
+export DATABASE_URL="${DATABASE_URL:-postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}}"
+
 python manage.py migrate --noinput
 watchmedo auto-restart --directory=./  --pattern=""*.py"" --recursive -- python manage.py runserver 0.0.0.0:8000 --noreload
