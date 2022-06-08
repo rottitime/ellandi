e54bd3534900f6e2d35141bb16f840b103f59087
Nina Menezes <nina.menezes@example.com>
2022-06-08 16:13:14 +0100

    add more tests for endpoints for users

--- api/tests/registration/test_endpoints.py
+++ api/tests/registration/test_endpoints.py
@@ -1,6 +1,9 @@
-from re import L
+import json
+
 from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
+
+from ellandi.registration.models import User
 
 
 class TestUserEndpoint(APITestCase):
@@ -21,14 +24,25 @@ class TestUserEndpoint(APITestCase):
             "last_name": "Smith",
             "organisation": "Cabinet Office",
         }
+        self.user = User.objects.create(
+            email="jane@example.com", first_name="Jane", last_name="Green", organisation="DfE"
+        )
+        self.user_id = User.objects.get(email="jane@example.com").id
 
     def test_get(self):
         response = self.client.get("/users/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_get_user(self):
+        response = self.client.get(f"/users/{self.user_id}/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["first_name"], "Jane")
+
     def test_post(self):
         response = self.client.post("/users/", self.data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        user = User.objects.get(email="bob@example.com")
+        self.assertIsInstance(user, User)
 
     def test_post_no_email(self):
         response = self.client.post("/users/", self.data_incorrect)
