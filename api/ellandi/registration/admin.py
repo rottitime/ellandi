4792aeaea4576a64e0b9854ade8c721586ddf5eb
Nina Menezes <nina.menezes@example.com>
2022-06-08 14:05:47 +0100

    add fieldsets to admin

--- api/ellandi/registration/admin.py
+++ api/ellandi/registration/admin.py
@@ -6,6 +6,40 @@ from .models import User
 
 class UserAdmin(django.contrib.auth.admin.UserAdmin):
     ordering = ("email",)
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "organisation",
+                    "job_title",
+                    "line_manager_email",
+                    "country",
+                    "contract_type",
+                )
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "organisation",
+                    "job_title",
+                    "line_manager_email",
+                    "country",
+                    "contract_type",
+                )
+            },
+        ),
+    )
 
 
 admin.site.register(User, UserAdmin)
