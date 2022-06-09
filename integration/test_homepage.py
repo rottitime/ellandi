5ba3eed8cb0dce2ab21aa7cebd8e027db86747f3
Ed Singleton <singletoned@gmail.com>
2022-06-09 09:39:03 +0100

    Use correct url

--- integration/test_homepage.py
+++ integration/test_homepage.py
@@ -3,6 +3,6 @@ from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
     browser = p.chromium.launch()
     page = browser.new_page()
-    page.goto("http://playwright.dev")
+    page.goto("http://localhost:3000/")
     print(page.title())
     browser.close()
