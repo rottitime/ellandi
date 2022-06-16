d25cb62a438c28e07c18d4b53749a891db6bc713
Ed Singleton <singletoned@gmail.com>
2022-06-16 09:24:56 +0100

    Delete users migration

---
+++ api/ellandi/registration/migrations/0014_delete_users.py
@@ -0,0 +1,14 @@
+from django.db import migrations
+
+def delete_users(apps, schema_editor):
+    User = apps.get_model('registration', 'User')
+    User.objects.all().delete()
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("registration", "0013_alter_userskill_level"),
+    ]
+
+    operations = [
+        migrations.RunPython(delete_users)
+    ]
