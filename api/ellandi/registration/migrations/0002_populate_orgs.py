4a9f766a2784d53d88f9d9c22b301b3918e7b740
Nina Menezes <nina.menezes@example.com>
2022-06-16 13:06:48 +0100

    update migration for population orgs

--- api/ellandi/registration/migrations/0002_populate_orgs.py
+++ api/ellandi/registration/migrations/0002_populate_orgs.py
@@ -7,11 +7,11 @@ from django.utils.text import slugify
 def populate_orgs(apps, schema_editor):
     Organisation = apps.get_model("registration", "Organisation")
     orgs = []
-    with open("ellandi/registration/migrations/0018_populate_orgs.txt") as file:
+    with open("ellandi/registration/migrations/0002_populate_orgs.txt") as file:
         for line in file:
             line = line.strip()
             slug = slugify(line)
-            org = Organisation(organisation=line, org_slug=slug)
+            org = Organisation(name=line, slug=slug)
             orgs.append(org)
     Organisation.objects.bulk_create(orgs)
 
