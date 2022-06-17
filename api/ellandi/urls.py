847d24960151babfe68753a1fe70470e014bd15b
Ed Singleton <singletoned@gmail.com>
2022-06-17 10:35:36 +0100

    Add drf-spectacular to urls

--- api/ellandi/urls.py
+++ api/ellandi/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView)
 
 from ellandi.registration import views
 
@@ -13,8 +14,15 @@ router.register(r"contract-types", views.ContractTypeViewSet)
 router.register(r"locations", views.LocationViewSet)
 router.register(r"languages", views.LanguageViewSet)
 
+schema_urlpatterns = [
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+]
+
 urlpatterns = [
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("admin/", admin.site.urls),
-]
+] + schema_urlpatterns
