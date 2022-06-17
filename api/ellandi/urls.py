da2b4a468b0eb1e2512ab8ca5bf72d2b38b642e6
Ed Singleton <singletoned@gmail.com>
2022-06-17 10:35:24 +0100

    Delete comments from urls

--- api/ellandi/urls.py
+++ api/ellandi/urls.py
@@ -13,8 +13,6 @@ router.register(r"contract-types", views.ContractTypeViewSet)
 router.register(r"locations", views.LocationViewSet)
 router.register(r"languages", views.LanguageViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
