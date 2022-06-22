340b2a06d4d191e20228665014b9e6739fb7530c
rottitime <1698584+rottitime@users.noreply.github.com>
2022-06-22 13:45:52 +0100

    local fix for GE

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 
 cat > ../.env <<EOF
-NODE_ENV="$NODE_ENV"
+NODE_ENV="production"
 VITE_FRONTEND_HOST="$VITE_FRONTEND_HOST"
 VITE_PROXY_ROOT_URL="$VITE_PROXY_ROOT_URL"
 VITE_API_ROOT_URL="$VITE_API_ROOT_URL"
