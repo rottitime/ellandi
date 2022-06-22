1b765b67786850f829c597d3dd18c124d9b9ea54
Ed Singleton <singletoned@gmail.com>
2022-06-22 09:57:47 +0100

    Add page 8 with contract types

--- api/ellandi/views.py
+++ api/ellandi/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 from ellandi.registration import models
 
 
-
-
 def page_view(request, page_num):
     grades = tuple({'value': grade.slug, 'text': grade.name} for grade in models.Grade.objects.all())
     professions = tuple({'value': profession.slug, 'text': profession.name} for profession in models.Profession.objects.all())
+    contract_types = tuple({'value': contract_type.slug, 'text': contract_type.name} for contract_type in models.ContractType.objects.all())
+
     return render(
         request,
         template_name=f"page{page_num}.html",
-        context={'grades': grades, 'professions': professions},
+        context={'grades': grades, 'professions': professions, 'contract_types': contract_types},
     )
