faa733511f669bb98af539f9a30507250010285f
Ed Singleton <singletoned@gmail.com>
2022-06-22 14:36:16 +0100

    Add page 11, languages

--- api/ellandi/views.py
+++ api/ellandi/views.py
@@ -11,9 +11,10 @@ def page_view(request, page_num):
     contract_types = tuple(
         {"value": contract_type.slug, "text": contract_type.name} for contract_type in models.ContractType.objects.all()
     )
+    languages = tuple({"value": language.slug, "text": language.name} for language in models.Language.objects.all())
 
     return render(
         request,
         template_name=f"page{page_num}.html",
-        context={"grades": grades, "professions": professions, "contract_types": contract_types},
+        context={"grades": grades, "professions": professions, "contract_types": contract_types,"languages": languages,},
     )
