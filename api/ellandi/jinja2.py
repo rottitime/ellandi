6680b5ea62fff1ee8e01a087d973fe6d0e8f6423
Ed Singleton <singletoned@gmail.com>
2022-06-22 07:16:12 +0100

    Fix jinja config

--- api/ellandi/jinja2.py
+++ api/ellandi/jinja2.py
@@ -1,15 +1,28 @@
-import govuk_frontend_jinja
 import jinja2
 from django.templatetags.static import static
 from django.urls import reverse
 
 
 def environment(**options):
+    extra_options = dict(
+        loader=jinja2.ChoiceLoader(
+            [
+                options["loader"],
+                jinja2.PrefixLoader(
+                    {
+                        "govuk_frontend_jinja": jinja2.PackageLoader(
+                            "govuk_frontend_jinja"
+                        )
+                    }
+                ),
+            ]
+        )
+    )
     env = jinja2.Environment(
-        jinja2.select_autoescape(("html", "htm", "xml", "xhtml", "njk")),
-        extensions=[govuk_frontend_jinja.templates.NunjucksExtension],
-        undefined=govuk_frontend_jinja.templates.NunjucksUndefined,
-        # **options,
+        **{
+            **options,
+            **extra_options,
+        }
     )
     env.globals.update(
         {
