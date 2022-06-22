158b4974fea1f58fa7bcdcdd44662562c2c10b5d
Ed Singleton <singletoned@gmail.com>
2022-06-22 09:53:09 +0100

    Add page 7 with professions

--- api/ellandi/views.py
+++ api/ellandi/views.py
@@ -7,8 +7,9 @@ from ellandi.registration import models
 
 def page_view(request, page_num):
     grades = tuple({'value': grade.slug, 'text': grade.name} for grade in models.Grade.objects.all())
+    professions = tuple({'value': profession.slug, 'text': profession.name} for profession in models.Profession.objects.all())
     return render(
         request,
         template_name=f"page{page_num}.html",
-        context={'grades': grades},
+        context={'grades': grades, 'professions': professions},
     )
