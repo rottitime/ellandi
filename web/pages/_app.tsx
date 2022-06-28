0d71ed80dcec1ee54a244e47e368dcc300c8f432
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-06-28 12:05:32 +0100

    temp

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,34 +1,14 @@
-import * as React from 'react'
-import Head from 'next/head'
-import { AppProps } from 'next/app'
-import { ThemeProvider } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
-import { CacheProvider, EmotionCache } from '@emotion/react'
+import { GlobalStyle } from 'govuk-react'
+import { ThemeProvider } from 'styled-components'
 import theme from '@/style/theme'
-import createEmotionCache from '@/style/createEmotionCache'
-import { SnackbarProvider } from 'notistack'
 
-// Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache()
-
-interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache
-}
-
-export default function MyApp(props: MyAppProps) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+export default function App({ Component, pageProps }) {
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
+    <>
+      <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <SnackbarProvider maxSnack={10}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <Component {...pageProps} />
-        </SnackbarProvider>
+        <Component {...pageProps} />
       </ThemeProvider>
-    </CacheProvider>
+    </>
   )
 }
--- web/style/theme.ts
+++ web/style/theme.ts
@@ -1,84 +1,13 @@
-import { createTheme } from '@mui/material/styles'
-
-const breakpoints = {
-  xs: 0,
-  md: 767,
-  lg: 1023,
-  xl: 1399
-}
-
-declare module '@mui/material/Typography' {
-  interface TypographyPropsVariantOverrides {
-    // small: true
-    h4: false
-    h5: false
-    h6: false
-    subtitle2: false
-    body2: false
-    overline: false
+interface ThemeInterface {
+  colors: {
+    primary: string
   }
 }
 
-declare module '@mui/material/styles' {
-  interface BreakpointOverrides {
-    sm: false
+const theme: ThemeInterface = {
+  colors: {
+    primary: '#0070f3'
   }
 }
-
-const theme = createTheme({
-  breakpoints: {
-    values: breakpoints
-  },
-  palette: {
-    background: {
-      default: 'rgb(242, 242, 242)'
-    }
-  },
-  typography: {
-    h1: {
-      fontSize: '32px',
-      lineHeight: '32px',
-      '&.MuiTypography-gutterBottom': {
-        marginBottom: 50
-      }
-    },
-    h2: {
-      fontSize: '24px',
-      lineHeight: '32px'
-    },
-    h3: {
-      fontSize: '16px',
-      lineHeight: '24px'
-    },
-    body1: {
-      fontSize: '16px',
-      lineHeight: '24px',
-      '&.MuiTypography-gutterBottom': {
-        marginBottom: 16
-      }
-    },
-    caption: {
-      fontSize: '14px',
-      lineHeight: '24px'
-    },
-    h4: undefined,
-    h5: undefined,
-    h6: undefined,
-    subtitle2: undefined,
-    body2: undefined,
-    overline: undefined
-  },
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: {
-        body: {
-          //background: 'url(/images/test/bg1.webp) bottom center  no-repeat',
-          //backgroundSize: 'cover',
-          minHeight: '100vh'
-        }
-      }
-    }
-  }
-})
 
 export default theme
