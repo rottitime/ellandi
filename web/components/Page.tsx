5e4fed7cb3fec952499362278555b9d813c476f7
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-06-29 22:44:42 +0100

    speedidal

---
+++ web/components/Page.tsx
@@ -0,0 +1,39 @@
+import { FC, ReactNode } from 'react'
+import { Container as MuiContainer } from '@mui/material'
+import AppBar from '@/components/AppBar'
+import { styled } from '@mui/material/styles'
+import Drawer from '@/components/Drawer'
+import BasicSpeedDial from './SpeedDial'
+
+const Container = styled(MuiContainer)`
+  padding-top: 20px;
+`
+
+type Props = {
+  children: ReactNode
+}
+
+const Page: FC<Props> = ({ children, ...props }) => (
+  <>
+    <AppBar />
+    <Drawer />
+    <BasicSpeedDial sx={{ position: 'fixed', bottom: '30px', right: '30px;' }} />
+    <Container maxWidth="xl" {...props}>
+      {children}
+      {/* <Grid container spacing={4}>
+        <Grid item xs={2}>
+          <Paper variant="outlined" sx={{ height: '100%' }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum reprehenderit
+            ex, rem quo adipisci quaerat eaque qui a reiciendis odit blanditiis
+            consequatur deleniti voluptatibus expedita, officia voluptas in, eveniet ea?
+          </Paper>
+        </Grid>
+        <Grid item xs={10}>
+          {children}
+        </Grid>
+      </Grid> */}
+    </Container>
+  </>
+)
+
+export default Page
