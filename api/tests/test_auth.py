049cd1038674aca9621efc1a4de314347837950f
Ed Singleton <singletoned@gmail.com>
2022-07-01 17:13:07 +0100

    Factored out with_client

--- api/tests/test_auth.py
+++ api/tests/test_auth.py
@@ -1,21 +1,32 @@
+import functools
+
 import httpx
+
 
 from ellandi import wsgi
 from ellandi.registration.models import User
 
 
-def test_homepage_no_auth():
-    with httpx.Client(app=wsgi.application, base_url="http://testserver:8000") as client:
-        response = client.get("/")
-        assert response.status_code == 401
-        assert response.json() == {"detail":"Authentication credentials were not provided."}
+def with_client(func):
+    @functools.wraps(func)
+    def _inner(*args, **kwargs):
+        with httpx.Client(app=wsgi.application, base_url="http://testserver:8000") as client:
+            return func(client, *args, **kwargs)
+    return _inner
 
 
-def test_login():
+@with_client
+def test_homepage_no_auth(client):
+    response = client.get("/")
+    assert response.status_code == 401
+    assert response.json() == {"detail":"Authentication credentials were not provided."}
+
+
+@with_client
+def test_login(client):
     data = dict(email="mr_flibble@example.com", password="P455w0rd")
     User.objects.create_user(**data)
 
-    with httpx.Client(app=wsgi.application, base_url="http://testserver:8000") as client:
-        response = client.post("/login/", json={'username': data['email'], "password": data['password']})
-        assert response.status_code == 200
-        assert response.json()['token']
+    response = client.post("/login/", json={'username': data['email'], "password": data['password']})
+    assert response.status_code == 200
+    assert response.json()['token']
