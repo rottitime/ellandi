95663c3627e7f7fec1595abc290fc5ed02be2cbd
Ed Singleton <singletoned@gmail.com>
2022-07-01 16:36:35 +0100

    Add test for logging in

--- api/tests/test_auth.py
+++ api/tests/test_auth.py
@@ -1,6 +1,7 @@
 import httpx
 
 from ellandi import wsgi
+from ellandi.registration.models import User
 
 
 def test_homepage_no_auth():
@@ -8,3 +9,13 @@ def test_homepage_no_auth():
         response = client.get("/")
         assert response.status_code == 401
         assert response.json() == {"detail":"Authentication credentials were not provided."}
+
+
+def test_login():
+    data = dict(email="mr_flibble@example.com", password="P455w0rd")
+    User.objects.create_user(**data)
+
+    with httpx.Client(app=wsgi.application, base_url="http://testserver:8000") as client:
+        response = client.post("/login/", json={'username': data['email'], "password": data['password']})
+        assert response.status_code == 200
+        assert response.json()['token']
