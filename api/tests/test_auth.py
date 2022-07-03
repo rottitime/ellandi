35116ccdc1ca418a7b40c1ba9460ff06770f0a6d
Ed Singleton <singletoned@gmail.com>
2022-07-03 10:09:00 +0100

    Add test for logout

--- api/tests/test_auth.py
+++ api/tests/test_auth.py
@@ -32,6 +32,25 @@ def test_homepage_no_auth(client):
 
 
 @with_client
+def test_logout(client):
+
+    response = client.post("/login/", json={'username': user_data['email'], "password": user_data['password']})
+    assert response.status_code == 200
+    token = response.json()['token']
+    assert token
+
+    headers = {'Authorization': f"Token {token}"}
+    client.headers = headers
+
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json()['users'] == "http://testserver:8000/users/"
+
+    response = client.post("/logout/")
+    assert response.status_code == 204
+
+
+@with_client
 def test_login(client):
     response = client.post("/login/", json={'username': user_data['email'], "password": user_data['password']})
     assert response.status_code == 200
