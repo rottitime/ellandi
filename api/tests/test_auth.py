430fd967cabd35273439e387a0ff28ab9197ad65
Ed Singleton <singletoned@gmail.com>
2022-07-04 15:21:56 +0100

    Factor out utils

--- api/tests/test_auth.py
+++ api/tests/test_auth.py
@@ -1,9 +1,5 @@
-import functools
-
-import httpx
-
-from ellandi import wsgi
 from ellandi.registration.models import User
+from tests import utils
 
 user_data = dict(email="mr_flibble@example.com", password="P455w0rd")
 
@@ -16,23 +12,14 @@ def teardown():
     User.objects.filter(email=user_data["email"]).delete()
 
 
-def with_client(func):
-    @functools.wraps(func)
-    def _inner(*args, **kwargs):
-        with httpx.Client(app=wsgi.application, base_url="http://testserver:8000") as client:
-            return func(client, *args, **kwargs)
-
-    return _inner
-
-
-@with_client
+@utils.with_client
 def test_homepage_no_auth(client):
     response = client.get("/")
     assert response.status_code == 401
     assert response.json() == {"detail": "Authentication credentials were not provided."}
 
 
-@with_client
+@utils.with_client
 def test_logout(client):
 
     response = client.post("/login/", json={"username": user_data["email"], "password": user_data["password"]})
@@ -51,7 +38,7 @@ def test_logout(client):
     assert response.status_code == 204
 
 
-@with_client
+@utils.with_client
 def test_login(client):
     response = client.post("/login/", json={"username": user_data["email"], "password": user_data["password"]})
     assert response.status_code == 200
---
+++ api/tests/utils.py
@@ -0,0 +1,14 @@
+import functools
+
+import httpx
+
+from ellandi import wsgi
+
+
+def with_client(func):
+    @functools.wraps(func)
+    def _inner(*args, **kwargs):
+        with httpx.Client(app=wsgi.application, base_url="http://testserver:8000") as client:
+            return func(client, *args, **kwargs)
+
+    return _inner
