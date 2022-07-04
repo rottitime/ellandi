d8755174bdf7a904a8b42e6cccbe425ce119a02a
Ed Singleton <singletoned@gmail.com>
2022-07-04 19:32:44 +0100

    Add tests for register view

--- api/tests/test_auth.py
+++ api/tests/test_auth.py
@@ -2,6 +2,7 @@ from ellandi.registration.models import User
 from tests import utils
 
 user_data = dict(email="mr_flibble@example.com", password="P455w0rd")
+new_user_data = dict(email="someone_new@example.com", password="0th3rP455w0rd")
 
 
 def setup():
@@ -10,6 +11,22 @@ def setup():
 
 def teardown():
     User.objects.filter(email=user_data["email"]).delete()
+
+
+@utils.with_client
+def test_create_user_already_exists(client):
+    response = client.post("/register/", json={"email": user_data["email"], "password": user_data["password"]})
+    assert response.status_code == 400
+    error = response.json()
+    assert error == {"detail":"User already exists"}
+
+
+@utils.with_client
+def test_create_user(client):
+    response = client.post("/register/", json={"email": new_user_data["email"], "password": new_user_data["password"]})
+    assert response.status_code == 200
+    user = response.json()
+    assert user['id']
 
 
 @utils.with_client
