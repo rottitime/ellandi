58ecc79cb921752a690f227a6210d10f13ab6fc7
Ed Singleton <singletoned@gmail.com>
2022-07-05 17:19:07 +0100

    Add with_logged_in_client to utils

--- api/tests/utils.py
+++ api/tests/utils.py
@@ -3,6 +3,15 @@ import functools
 import httpx
 
 from ellandi import wsgi
+from ellandi.registration.models import User, UserSkill
+
+user_data = dict(
+    email="jane@example.com",
+    first_name="Jane",
+    last_name="Green",
+    organisation="DfE",
+    password="P455w0rd",
+)
 
 
 def with_client(func):
@@ -10,5 +19,27 @@ def with_client(func):
     def _inner(*args, **kwargs):
         with httpx.Client(app=wsgi.application, base_url="http://testserver:8000") as client:
             return func(client, *args, **kwargs)
+
+    return _inner
+
+
+def with_logged_in_client(func):
+    @functools.wraps(func)
+    def _inner(*args, **kwargs):
+        user = User.objects.create_user(**user_data)
+        UserSkill.objects.create(user=user, skill_name="Python", level="beginner")
+
+        with httpx.Client(app=wsgi.application, base_url="http://testserver:8000") as client:
+            response = client.post("/login/", json={"username": user_data["email"], "password": user_data["password"]})
+            assert response.status_code == 200
+            token = response.json()["token"]
+            assert token
+
+            headers = {"Authorization": f"Token {token}"}
+            client.headers = headers
+            try:
+                return func(client, user.id, *args, **kwargs)
+            finally:
+                User.objects.filter(id=user.id).delete()
 
     return _inner
