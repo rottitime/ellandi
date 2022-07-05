749dddf0f7ec0b97506b78ba79aed0d2b5a9a475
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-07-05 11:32:59 +0100

    index navigation page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,42 +1,28 @@
 import Template from '@/components/Layout/Template'
-import Link from '@/components/UI/Link'
-import { Typography } from '@mui/material'
+import LinkButton from '@/components/LinkButton'
+import Card from '@/components/UI/Card'
+import { Divider, Grid, Typography } from '@mui/material'
 
 const IndexPage = () => (
-  <>
-    <Typography variant="h1" gutterBottom>
-      Hi <Link href="#">joe.bloggs@cabinetoffice.gov.uk</Link>
-    </Typography>
-
-    <Typography gutterBottom>
-      You are invited to register on Civil Service Skills. Please select the following
-      link to sign up:
-    </Typography>
-
-    <Typography gutterBottom>
-      <Link href="/register/page2">
-        http://skills.civilservice.gov.uk/signup/123AbcDefgh1238910ABCdefghk
-      </Link>
-    </Typography>
-    <Typography gutterBottom>
-      The above is a one-time-only link; you can only use this link once. If you use this
-      link more than once, your invitation expires and you will not be able to sign up to
-      Civil Service Skills.
-    </Typography>
-    <Typography gutterBottom>
-      Please contact{' '}
-      <Link href="mailto:mailto:support.learn@csskills.gov.uk">
-        support.learn@csskills.gov.uk
-      </Link>{' '}
-      if you need the sign-up link to be resent.
-    </Typography>
-
-    <Typography gutterBottom>
-      Regards
-      <br />
-      skills.civilservice Team
-    </Typography>
-  </>
+  <Grid container gap={5}>
+    <Grid item xs>
+      <Card>
+        <Typography variant="h2" gutterBottom>
+          Register
+        </Typography>
+        <LinkButton href="/register">Register</LinkButton>
+      </Card>
+    </Grid>
+    <Divider orientation="vertical" flexItem></Divider>
+    <Grid item xs>
+      <Card>
+        <Typography variant="h2" gutterBottom>
+          Sign in
+        </Typography>
+        <LinkButton href="/signin">Sign in</LinkButton>
+      </Card>
+    </Grid>
+  </Grid>
 )
 
 export default IndexPage
