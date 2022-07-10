7cdfc42600f4a4118c22db6243d918b43f55f2bf
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-07-10 19:32:32 +0100

    spacing fix for registration pages

--- web/components/Layout/AccountMenuPage.tsx
+++ web/components/Layout/AccountMenuPage.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode } from 'react'
-import { Breadcrumbs, Typography } from '@mui/material'
+import { Box, Breadcrumbs, Typography } from '@mui/material'
 import AppBar from '@/components/UI/AppBar/AppBar'
 import Link from '@/components/UI/Link'
 import Template from './Template'
@@ -25,23 +25,25 @@ const settings = [
 
 const AccountMenuPage: FC<Props> = ({ breadcrumbs, title, children }) => (
   <Template header={<AppBar pages={pages} settings={settings} />}>
-    {breadcrumbs && (
-      <Breadcrumbs aria-label="breadcrumb" sx={{ mb: 3 }}>
-        <Link underline="hover" color="inherit" href="/account">
-          Home
-        </Link>
+    <Box component="header" sx={{ m: 3 }}>
+      {breadcrumbs && (
+        <Breadcrumbs aria-label="breadcrumb" sx={{ mb: 3 }}>
+          <Link underline="hover" color="inherit" href="/account">
+            Home
+          </Link>
 
-        {breadcrumbs.map((item) =>
-          item.url ? (
-            <Link underline="hover" color="inherit" href={item.url} key={item.title}>
-              {item.title}
-            </Link>
-          ) : (
-            <Typography color="text.primary">{item.title}</Typography>
-          )
-        )}
-      </Breadcrumbs>
-    )}
+          {breadcrumbs.map((item) =>
+            item.url ? (
+              <Link underline="hover" color="inherit" href={item.url} key={item.title}>
+                {item.title}
+              </Link>
+            ) : (
+              <Typography color="text.primary">{item.title}</Typography>
+            )
+          )}
+        </Breadcrumbs>
+      )}
+    </Box>
 
     <Typography variant="h1" gutterBottom>
       {title}
