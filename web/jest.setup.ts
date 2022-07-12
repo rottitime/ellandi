5f2deaa30e52acd40207d2822b8b254374922b9b
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-07-12 22:25:27 +0100

    test config variables

--- web/jest.setup.ts
+++ web/jest.setup.ts
@@ -5,3 +5,7 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
 import '@testing-library/jest-dom/extend-expect'
+
+jest.mock('next/config', () => () => ({
+  publicRuntimeConfig: {}
+}))
