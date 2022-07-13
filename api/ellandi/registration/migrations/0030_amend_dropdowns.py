039fc6d331948cacaed4c75d172e2e77bf935e5b
Nina Menezes <nina.menezes@example.com>
2022-07-13 12:47:00 +0100

    amend content of some drop-downs

---
+++ api/ellandi/registration/migrations/0030_amend_dropdowns.py
@@ -0,0 +1,28 @@
+"""Make changes to existing drop-downs"""
+
+from django.db import migrations
+from django.utils.text import slugify
+
+
+def add_profession(apps, schema_editor):
+    Profession = apps.get_model("registration", "Profession")
+    name = "Medical Profession"
+    medical_profession = Profession(name=name, slug=slugify(name))
+    medical_profession.save()
+
+
+def change_grade(apps, schema_editor):
+    Grade = apps.get_model("registration", "Grade")
+    other_grade = Grade.objects.get(slug="other-equivalent-grade")
+    other_grade.name = "Other"
+    other_grade.slug = "other"
+    other_grade.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("registration", "0029_populate_countries"),
+    ]
+
+    operations = [migrations.RunPython(add_profession), migrations.RunPython(change_grade)]
