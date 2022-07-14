d0419e008eb3e04b6a1b2fc02666ac20e24bfbc2
Nina Menezes <nina.menezes@example.com>
2022-07-14 14:34:58 +0100

    add to docker instructions

--- docker/api/start.sh
+++ docker/api/start.sh
@@ -4,4 +4,8 @@ set -o errexit
 set -o nounset
 
 python manage.py migrate --noinput
+# python manage.py loaddata dropdown/*.json
+python manage.py loaddata dropdown/countries.json
+python manage.py loaddata dropdown/grades.json
+
 watchmedo auto-restart --directory=./  --pattern=""*.py"" --recursive -- python manage.py runserver 0.0.0.0:8000 --noreload
