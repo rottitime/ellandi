ad03a289d362de947cb392ce3a618ea06d593669
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-07-18 15:25:35 +0100

    Test update: Test page with Router mock
    router push test

--- web/__tests__/hello.test.tsx
+++ web/__tests__/hello.test.tsx
@@ -1,9 +1,14 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import Home from '@/pages/hello'
 import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+import Router from 'next/router'
+import { renderWithProviders, renderWithProviders2 } from '@/lib/test-utils'
+
+jest.mock('next/router', () => ({ push: jest.fn() }))
 
 describe('Home', () => {
-  it('renders a heading', () => {
+  it('renders', () => {
     render(<Home />)
 
     const heading = screen.getByRole('heading', {
@@ -12,5 +17,33 @@ describe('Home', () => {
     })
 
     expect(heading).toBeInTheDocument()
+    expect(screen.getByTestId('button')).toBeInTheDocument()
+    expect(screen.getByTestId('route-button')).toBeInTheDocument()
+    expect(screen.getByTestId('output')).toBeInTheDocument()
+  })
+  it('button press', async () => {
+    render(<Home />)
+    const button = screen.getByTestId('button')
+
+    expect(screen.getByTestId('output')).toBeEmptyDOMElement()
+
+    userEvent.click(button)
+
+    await waitFor(async () => {
+      expect(screen.getByTestId('output')).toHaveTextContent('done')
+    })
+  })
+
+  it('route spy', async () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+
+    renderWithProviders(<Home />)
+    const button = screen.getByTestId('route-button')
+
+    userEvent.click(button)
+
+    await waitFor(async () => {
+      expect(Router.push).toHaveBeenCalledTimes(1)
+    })
   })
 })
--- web/pages/hello.tsx
+++ web/pages/hello.tsx
@@ -1,65 +1,92 @@
 import { Typography, Box } from '@mui/material'
+import GenericPage from '@/components/Layout/GenericPage'
+import router from 'next/router'
+import { useState } from 'react'
 
-const HelloPage = () => (
-  <>
-    <Box sx={{ width: '100%', maxWidth: 500 }}>
-      <p>NEXT_PUBLIC_ANALYTICS_ID: {process.env.NEXT_PUBLIC_ANALYTICS_ID}</p>
-      <p>NODE_ENV: {process.env.NODE_ENV}</p>
-      <p>NEXT_PUBLIC_API_URL: {process.env.NEXT_PUBLIC_API_URL}</p>
+const HelloPage = () => {
+  const [output, setOutput] = useState('')
 
-      <Typography variant="leader" gutterBottom>
-        Leader Heading
-      </Typography>
+  return (
+    <>
+      <Box sx={{ width: '100%', maxWidth: 500 }}>
+        <p>NEXT_PUBLIC_ANALYTICS_ID: {process.env.NEXT_PUBLIC_ANALYTICS_ID}</p>
+        <p>NODE_ENV: {process.env.NODE_ENV}</p>
+        <p>NEXT_PUBLIC_API_URL: {process.env.NEXT_PUBLIC_API_URL}</p>
 
-      <Typography variant="h1" gutterBottom>
-        h1. Heading
-      </Typography>
-      <Typography variant="h2" gutterBottom>
-        h2. Heading
-      </Typography>
-      <Typography variant="h3" gutterBottom>
-        h3. Heading
-      </Typography>
-      {/* <Typography variant="h4" gutterBottom >
-        h4. Heading
-      </Typography>
-      <Typography variant="h5" gutterBottom >
-        h5. Heading
-      </Typography>
-      <Typography variant="h6" gutterBottom >
-        h6. Heading
-      </Typography> */}
-      <Typography variant="subtitle1" gutterBottom>
-        subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-        blanditiis tenetur
-      </Typography>
-      {/* <Typography variant="subtitle2" gutterBottom >
-        subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-        blanditiis tenetur
-      </Typography> */}
-      <Typography variant="body1" gutterBottom>
-        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis
-        tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus,
-        cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem
-        quibusdam.
-      </Typography>
-      <Typography variant="body2" gutterBottom>
-        body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis
-        tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus,
-        cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem
-        quibusdam.
-      </Typography>
-      <Typography variant="button" display="block" gutterBottom>
-        button text
-      </Typography>
-      <Typography variant="caption" display="block" gutterBottom>
-        caption text
-      </Typography>
-      {/* <Typography variant="overline" display="block" gutterBottom>
-        overline text
-      </Typography> */}
-    </Box>
-  </>
-)
+        <Typography variant="leader" gutterBottom>
+          Leader Heading
+        </Typography>
+
+        <Typography variant="h1" gutterBottom>
+          h1. Heading
+        </Typography>
+        <Typography variant="h2" gutterBottom>
+          h2. Heading
+        </Typography>
+        <Typography variant="h3" gutterBottom>
+          h3. Heading
+        </Typography>
+        {/* <Typography variant="h4" gutterBottom >
+    h4. Heading
+  </Typography>
+  <Typography variant="h5" gutterBottom >
+    h5. Heading
+  </Typography>
+  <Typography variant="h6" gutterBottom >
+    h6. Heading
+  </Typography> */}
+        <Typography variant="subtitle1" gutterBottom>
+          subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+          blanditiis tenetur
+        </Typography>
+        {/* <Typography variant="subtitle2" gutterBottom >
+    subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+    blanditiis tenetur
+  </Typography> */}
+        <Typography variant="body1" gutterBottom>
+          body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis
+          tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus,
+          cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem
+          quibusdam.
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis
+          tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus,
+          cupiditate numquam dignissimos laborum fugiat deleniti? Eum quasi quidem
+          quibusdam.
+        </Typography>
+        <Typography variant="button" display="block" gutterBottom>
+          button text
+        </Typography>
+        <Typography variant="caption" display="block" gutterBottom>
+          caption text
+        </Typography>
+        {/* <Typography variant="overline" display="block" gutterBottom>
+    overline text
+  </Typography> */}
+
+        <button data-testid="button" onClick={() => setOutput('done')}>
+          test button
+        </button>
+
+        <button
+          data-testid="route-button"
+          onClick={() => {
+            router.push(`/register`)
+          }}
+        >
+          test Route
+        </button>
+        <div data-testid="output">{output}</div>
+      </Box>
+    </>
+  )
+}
 
 export default HelloPage
+
+HelloPage.getLayout = (page) => (
+  <GenericPage title="Your details" progress={20}>
+    {page}
+  </GenericPage>
+)
