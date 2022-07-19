d4e24c203f6548dde6ccfaca4d8e441bab97d4d2
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-07-19 16:46:49 +0100

    useAuth test with mock of SessionStorage

--- web/lib/test-utils.tsx
+++ web/lib/test-utils.tsx
@@ -3,6 +3,33 @@ import { render, RenderOptions } from '@testing-library/react'
 import { ReactNode } from 'react'
 import theme from '@/style/theme'
 
+beforeAll(() => {
+  Object.defineProperty(global, 'sessionStorage', { value: mockStorage })
+  Object.defineProperty(global, 'localStorage', { value: mockStorage })
+})
+
+afterEach(() => {
+  window.sessionStorage.clear()
+})
+
+const mockStorage = (() => {
+  let store = {}
+  return {
+    getItem: function (key) {
+      return store[key] || null
+    },
+    setItem: function (key, value) {
+      store[key] = value.toString()
+    },
+    removeItem: function (key) {
+      delete store[key]
+    },
+    clear: function () {
+      store = {}
+    }
+  }
+})()
+
 export const renderWithProviders = async (
   ui: ReactNode,
   options: Omit<RenderOptions, 'queries'> = {}
