4e4952e39528c37d31c9a8d2877c1c84e1ad4f00
Ed Singleton <singletoned@gmail.com>
2022-07-20 00:02:28 +0100

    Reorder migrations to, you know, work...

--- organogram/organogram/registration/migrations/0003_auto_20220719_1959.py
+++ organogram/organogram/registration/migrations/0003_auto_20220719_1959.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("registration", "0002_loaddata_fixtures"),
+        ("registration", "0001_initial"),
     ]
 
     operations = [
