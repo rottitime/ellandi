3c5cc1fa2e9ec1377bc3f81e1b67bce668d86a4d
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-07-19 16:21:29 +0100

    Tests: Create Account

--- web/components/Form/Register/CreateAccountForm.test.tsx
+++ web/components/Form/Register/CreateAccountForm.test.tsx
@@ -3,7 +3,10 @@ import userEvent from '@testing-library/user-event'
 import { renderWithProviders } from '@/lib/test-utils'
 import CreateAccountForm from '@/components/Form/Register/CreateAccountForm'
 
-describe.skip('CreateAccountForm', () => {
+describe('CreateAccountForm', () => {
+  const email = 'nonmatch@test.com'
+  const password = 'abc123456'
+
   it('renders', () => {
     renderWithProviders(<CreateAccountForm backUrl="/back" onFormSubmit={jest.fn()} />)
 
@@ -15,28 +18,29 @@ describe.skip('CreateAccountForm', () => {
     expect(screen.getByLabelText('Password')).toBeInTheDocument()
     expect(screen.getByLabelText('Confirm your password')).toBeInTheDocument()
   })
-  it('shows default errors', async () => {
+
+  it('errors for required fields', async () => {
     renderWithProviders(<CreateAccountForm backUrl="/back" onFormSubmit={jest.fn()} />)
 
     const button = screen.getByRole('button', { name: /Continue/i })
     expect(button).toBeInTheDocument()
 
-    userEvent.click(button)
+    await userEvent.click(button)
 
     await waitFor(async () => {
-      //expect(screen.getByTestId('errorhelper-email')).toBeInTheDocument()
-      expect(screen.getByTestId('errorhelper-email')).toBeInTheDocument()
-      //expect(screen.getAllByText('This is a required field')).toHaveLength(4)
+      expect(screen.getByTestId('field_email')).toHaveTextContent(
+        'This is a required field'
+      )
     })
 
-    // expect(screen.getAllByText('this is a required field')).toHaveLength(2)
-    // expect(screen.getByText('password must be at least 8 characters')).toBeInTheDocument()
+    expect(screen.getAllByText('This is a required field')).toHaveLength(3)
+    expect(screen.getByTestId('field_password')).toHaveTextContent(
+      'Password must be 8 characters or more'
+    )
   })
 
-  it('validates fields match', async () => {
-    renderWithProviders(<CreateAccountForm />)
-
-    const text = 'nonmatch@test.com'
+  it('error if field do not match', async () => {
+    renderWithProviders(<CreateAccountForm backUrl="/back" onFormSubmit={jest.fn()} />)
 
     const button = screen.getByRole('button', { name: /Continue/i })
     const emailField1 = screen.getByTestId('textfield_email')
@@ -44,21 +48,51 @@ describe.skip('CreateAccountForm', () => {
     const passwordField1 = screen.getByTestId('textfield_password')
     const passwordField2 = screen.getByTestId('textfield_passwordConfirm')
 
-    userEvent.type(emailField1, text)
-    userEvent.type(emailField2, text + '1')
-    userEvent.type(passwordField1, text)
-    userEvent.type(passwordField2, text + '1')
+    await userEvent.type(emailField1, email)
+    await userEvent.type(emailField2, email + '1')
+    await userEvent.type(passwordField1, password)
+    await userEvent.type(passwordField2, password + '1')
 
-    userEvent.click(button)
+    await userEvent.click(button)
 
     await waitFor(async () => {
-      expect(screen.getByText('Does not match email')).toBeInTheDocument()
+      expect(screen.getByTestId('field_emailConfirm')).toHaveTextContent(
+        'Does not match with email'
+      )
     })
 
-    // expect(screen.getAllByText('Does not match with password')).toHaveLength(2)
+    expect(screen.getByTestId('field_passwordConfirm')).toHaveTextContent(
+      'Does not match with password'
+    )
   })
 
-  it.skip('submits', async () => {
-    //tobe decided
+  it('submits', async () => {
+    const mockSubmit = jest.fn()
+
+    renderWithProviders(
+      <CreateAccountForm backUrl="/back" onFormSubmit={(data) => mockSubmit(data)} />
+    )
+
+    const button = screen.getByRole('button', { name: /Continue/i })
+    const emailField1 = screen.getByTestId('textfield_email')
+    const emailField2 = screen.getByTestId('textfield_emailConfirm')
+    const passwordField1 = screen.getByTestId('textfield_password')
+    const passwordField2 = screen.getByTestId('textfield_passwordConfirm')
+
+    await userEvent.type(emailField1, email)
+    await userEvent.type(emailField2, email)
+    await userEvent.type(passwordField1, password)
+    await userEvent.type(passwordField2, password)
+
+    await userEvent.click(button)
+
+    await waitFor(async () => {
+      expect(mockSubmit).toHaveBeenCalledWith({
+        email,
+        emailConfirm: email,
+        password,
+        passwordConfirm: password
+      })
+    })
   })
 })
