c74a9c30dc43b0eaea159917d43464c05460ddaf
Ed Singleton <singletoned@gmail.com>
2022-07-20 22:30:25 +0100

    Add primary_profession to user

--- organogram/organogram/registration/models.py
+++ organogram/organogram/registration/models.py
@@ -143,6 +143,7 @@ class RegistrationAbstractUser(models.Model):
     job_title = models.CharField(max_length=128, blank=True, null=True)
     grade = models.CharField(max_length=127, blank=True, null=False)
     professions = models.ManyToManyField(Profession, blank=True)
+    primary_profession = models.ForeignKey(Profession, related_name="+", blank=True, null=True, on_delete=models.CASCADE)
     contract_type = models.CharField(max_length=127, blank=True, null=False)
     line_manager_email = models.CharField(max_length=128, blank=True, null=True)
     location = models.CharField(max_length=127, blank=True, null=False)
