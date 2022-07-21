0e264c381b5fc8c60ed0364c7b0c9f156a549633
Ed Singleton <singletoned@gmail.com>
2022-07-21 11:25:15 +0100

    Tidy

--- organogram/organogram/registration/migrations/0005_merge_0003_user_photo_0004_user_primary_profession.py
+++ organogram/organogram/registration/migrations/0005_merge_0003_user_photo_0004_user_primary_profession.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('registration', '0003_user_photo'),
-        ('registration', '0004_user_primary_profession'),
+        ("registration", "0003_user_photo"),
+        ("registration", "0004_user_primary_profession"),
     ]
 
-    operations = [
-    ]
+    operations = []
