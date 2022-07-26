279b5edd373fa05c2c7a636d06777fed0edcc58d
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-07-26 17:26:38 +0100

    Mobile background optimise

--- web/style/global.ts
+++ web/style/global.ts
@@ -1,10 +1,12 @@
 import css from 'styled-jsx/css'
 
 export const withGovLogoBackground = css.global`
-  body {
-    background-image: url(/images/bg_crown.svg);
-    background-repeat: no-repeat;
-    background-size: 975px;
-    background-position: top right;
+  @media (min-width: 767px) {
+    body {
+      background-image: url(/images/bg_crown.svg);
+      background-repeat: no-repeat;
+      background-size: 975px;
+      background-position: top right;
+    }
   }
 `
