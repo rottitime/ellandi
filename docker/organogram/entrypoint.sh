1ef78fb3f9ab961ecb988187336f7974de1e66a3
Ed Singleton <singletoned@gmail.com>
2022-07-27 12:53:41 +0100

    Fix query in entrypoint

--- docker/organogram/entrypoint.sh
+++ docker/organogram/entrypoint.sh
@@ -3,6 +3,6 @@
 set -o errexit
 set -o nounset
 
-wait-for-db --mode postgres --connection-string $DATABASE_URL --timeout 60 --sql-query "select * from django_migrations;"
+wait-for-db --mode postgres --connection-string $DATABASE_URL --timeout 60 --sql-query "select 1;"
 
 exec "$@"
