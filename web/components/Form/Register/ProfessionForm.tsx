332b7caea71933ee365c4a34d54f6b8e2a03fbec
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-08-01 14:33:32 +0100

    Profession: integration with API and Form Component

--- web/components/Form/Register/ProfessionForm.tsx
+++ web/components/Form/Register/ProfessionForm.tsx
@@ -2,58 +2,42 @@ import { Alert, AlertTitle, Avatar, Box, Skeleton, Typography } from '@mui/mater
 import ToggleChip from '@/components/ToggleChip'
 import { fetchProfessions } from '@/service/api'
 import { GenericDataList, ProfessionType, Query } from '@/service/types'
-import { useUiContext } from '@/context/UiContext'
 import { useQuery } from 'react-query'
 import { FC, useEffect } from 'react'
-import FormFooter from '@/components/Form/FormFooter'
 import { StandardRegisterProps } from './types'
 import { FormProvider, useForm } from 'react-hook-form'
 import { array, object, SchemaOf, string } from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 import TextFieldControlled from '@/components/UI/TextFieldControlled/TextFieldControlled'
+import Form from '@/components/Form/Register/FormRegister/FormRegister'
 
 const fieldName: keyof ProfessionType = 'professions'
 
 const schema: SchemaOf<ProfessionType> = object().shape({
   professions: array().of(string()).min(1, 'This is a required field'),
-  profession_other: string().when('professions', (value) => {
-    if (value.includes('Other')) return string().required('This is a required field')
-  })
+  profession_other: string()
+    .nullable()
+    .when('professions', (value) => {
+      if (value.includes('Other'))
+        return string().nullable().required('This is a required field')
+    })
 })
 
-const ProfessionForm: FC<StandardRegisterProps<ProfessionType>> = ({
-  backUrl,
-  skipUrl,
-  onFormSubmit,
-  loading,
-  defaultValues = {
-    professions: [],
-    profession_other: ''
-  }
-}) => {
+const ProfessionForm: FC<StandardRegisterProps<ProfessionType>> = (props) => {
   const methods = useForm<ProfessionType>({
-    defaultValues,
+    defaultValues: {
+      professions: [],
+      profession_other: ''
+    },
     resolver: yupResolver(schema)
   })
 
-  const {
-    handleSubmit,
-    getValues,
-    setValue,
-    register,
-    trigger,
-    watch,
-    formState: { isDirty, isValid }
-  } = methods
+  const { getValues, setValue, register, trigger, watch } = methods
 
-  const { setLoading } = useUiContext()
   const { isLoading, isError, data } = useQuery<GenericDataList[], { message?: string }>(
     Query.Professions,
     fetchProfessions
   )
-  useEffect(() => {
-    setLoading(isLoading)
-  }, [isLoading, setLoading])
 
   useEffect(() => {
     if (!!watchFields.profession_other)
@@ -75,7 +59,7 @@ const ProfessionForm: FC<StandardRegisterProps<ProfessionType>> = ({
 
   return (
     <FormProvider {...methods}>
-      <form onSubmit={handleSubmit(onFormSubmit)} noValidate>
+      <Form {...props} submitDisabled>
         <Typography variant="subtitle1" gutterBottom>
           Select the Profession(s) that you belong to. You may choose more than one
         </Typography>
@@ -118,16 +102,7 @@ const ProfessionForm: FC<StandardRegisterProps<ProfessionType>> = ({
                 )}
               </Box>
             ))}
-
-        <FormFooter
-          skipUrl={skipUrl}
-          backUrl={backUrl}
-          buttonProps={{
-            loading,
-            disabled: !isDirty && !isValid
-          }}
-        />
-      </form>
+      </Form>
     </FormProvider>
   )
 }
