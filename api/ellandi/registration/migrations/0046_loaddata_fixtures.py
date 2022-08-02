52316408feae8696374ba615efeb085586f62dff
Nina Menezes <nina.menezes@example.com>
2022-08-02 20:43:34 +0100

    update fixtures

---
+++ api/ellandi/registration/migrations/0046_loaddata_fixtures.py
@@ -0,0 +1,23 @@
+"""Load all data from fixtures for drop-down lists."""
+
+from django.core.management import call_command
+from django.db import migrations
+
+MODELS_FIXTURES = {"grades": "Grade", "professions": "Profession", "functions": "Function"}
+
+
+def load_all_fixtures(apps, schema_editor):
+    for fixture_name, model_name in MODELS_FIXTURES.items():
+        model = apps.get_model("registration", model_name)
+        model.objects.all().delete()
+        fixture_file = f"dropdown/{fixture_name}.json"
+        call_command("loaddata", fixture_file, app_label="registration", ignorenonexistent=True)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("registration", "0045_auto_20220802_1921"),
+    ]
+
+    operations = [migrations.RunPython(load_all_fixtures)]
