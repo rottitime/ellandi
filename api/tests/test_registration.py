c43499f075c7a1a31c5b8049a451b7d069eafc79
Nina Menezes <nina.menezes@example.com>
2022-08-03 16:20:31 +0100

    shorten line

--- api/tests/test_registration.py
+++ api/tests/test_registration.py
@@ -72,7 +72,7 @@ def test_user_put(client, user_id):
     response_data = response.json()
     assert (
         response_data["email"] == "jane@example.com"
-    ), f"Email field should be read-only - value {response_data['email']}"
+    ), f"Email field should be read-only - {response_data['email']}"
     assert response.status_code == status.HTTP_200_OK
     assert response_data["last_name"] == "Brown"
     assert response_data["function"] == "Analysis"
