4c9c35c61af09701e886bafc295437530244bf6b
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-08-09 10:28:26 +0100

    props fixes

--- web/components/Accounts/Headline/types.ts
+++ web/components/Accounts/Headline/types.ts
@@ -3,5 +3,5 @@ import { Box } from '@mui/material'
 import { ComponentProps } from 'react'
 
 export type Props = {
-  textColor: keyof Colors | keyof ColorBrands
+  textColor?: keyof Colors | keyof ColorBrands
 } & ComponentProps<typeof Box>
