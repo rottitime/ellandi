a23914a318e5876627eebe80e515b39fcb5e72f8
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-08-09 20:07:23 +0100

    latest skills page

--- web/pages/account/skills/[[...tab]].tsx
+++ web/pages/account/skills/[[...tab]].tsx
@@ -1,175 +1,61 @@
 import AccountLayout from '@/components/Layout/AccountLayout/AccountLayout'
-import {
-  Box,
-  Chip,
-  styled,
-  Tab,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  Tabs as MuiTbas,
-  Typography
-} from '@mui/material'
-import { SyntheticEvent, useState } from 'react'
-import Link from '@/components/UI/Link'
-import Icon from '@/components/Icon/Icon'
+import { Box, Chip, IconButton, Typography } from '@mui/material'
 import Tabs, { TabItem } from '@/components/UI/Tabs/Tabs'
 import Button from '@/components/UI/Button/Button'
-import SkillsList from '@/components/Accounts/SkillsList/SkillsList'
 import useAuth from '@/hooks/useAuth'
 import { useQuery } from 'react-query'
 import { Query, RegisterUserResponse } from '@/service/api'
 import { fetchMe } from '@/service/me'
 import Headline from '@/components/Accounts/Headline/Headline'
+import { useUiContext } from '@/context/UiContext'
+import { useEffect } from 'react'
+import { GetStaticPropsContext } from 'next'
+import DataGrid, { GridColDef } from '@/components/UI/DataGrid/DataGrid'
+import Icon from '@/components/Icon/Icon'
 
-const TabPanel = styled(Box)`
-  padding: 25px;
-  .MuiTypography-h2 {
-    display: flex;
-    align-items: center;
-    gap: 10px;
-  }
-`
-
-const SkillsPage = () => {
+const SkillsPage = ({ tabIndex }) => {
+  const { setLoading } = useUiContext()
   const { authFetch } = useAuth()
-  const { data } = useQuery<RegisterUserResponse>(Query.Me, () => authFetch(fetchMe))
+  const { isLoading, data } = useQuery<RegisterUserResponse>(Query.Me, () =>
+    authFetch(fetchMe)
+  )
 
-  const [value, setValue] = useState(0)
-  const handleChange = (_: SyntheticEvent, newValue: number) => setValue(newValue)
+  useEffect(() => {
+    setLoading(isLoading)
+  }, [isLoading, setLoading])
 
   return (
     <>
       <Headline>
         <Typography variant="h1" gutterBottom>
-          Hello {data?.first_name}, welcome to the <br />
-          Skills and Learning Service
+          Hello {data?.first_name}, welcome to the Skills and Learning Service
         </Typography>
       </Headline>
 
-      <Box>
-        <Button variant="contained">Add a skill</Button>
+      <Button variant="contained" sx={{ mb: 4 }}>
+        Add a skill
+      </Button>
 
-        <Tabs tabItems={tabs} activeOnUrl />
-
-        <Box>
-          <MuiTbas value={value} onChange={handleChange} aria-label="basic tabs example">
-            <Tab label="Your skills" />
-            <Tab label="Language skills" />
-            <Tab label="Skills you'd like to develop" />
-          </MuiTbas>
-        </Box>
-        <TabPanel hidden={value !== 0}>
-          <Typography variant="h1" gutterBottom>
-            <Icon icon="skills" /> Skill
-          </Typography>
-
-          <Table sx={{ minWidth: 650 }} aria-label="simple table" size="small">
-            <TableHead>
-              <TableRow>
-                <TableCell>Skill</TableCell>
-                <TableCell>&nbsp;</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {[
-                'Collaboration',
-                'Customer service',
-                'Health and wellbeing',
-                'Independence',
-                'Job coaching',
-                'Market research',
-                'Risk management'
-              ].map((skill) => (
-                <TableRow key={skill}>
-                  <TableCell scope="row">{skill}</TableCell>
-
-                  <TableCell scope="row" align="right">
-                    <Link href="#">Remove</Link>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TabPanel>
-        <TabPanel hidden={value !== 1}>
-          <Typography variant="h1" gutterBottom>
-            {' '}
-            <Icon icon="skills" /> Language skills
-          </Typography>
-
-          <Table size="small">
-            <TableHead>
-              <TableRow>
-                <TableCell>Skill</TableCell>
-                <TableCell>Speaking</TableCell>
-                <TableCell>Writing</TableCell>
-                <TableCell>&nbsp;</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell>English</TableCell>
-                <TableCell>
-                  <Chip variant="outlined" label="PROFICIENT" />
-                </TableCell>
-                <TableCell>
-                  <Chip variant="outlined" label="PROFICIENT" />
-                </TableCell>
-                <TableCell>
-                  <Link href="#">Change</Link>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>French</TableCell>
-                <TableCell>
-                  <Chip variant="outlined" label="BASIC" />
-                </TableCell>
-                <TableCell>
-                  <Chip variant="outlined" label="INDEPENDENT" />
-                </TableCell>
-                <TableCell>
-                  <Link href="#">Change</Link>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TabPanel>
-        <TabPanel hidden={value !== 2}>
-          <Typography variant="h1" gutterBottom>
-            <Icon icon="skills" /> Skills you'd like to develop
-          </Typography>
-
-          <Table sx={{ minWidth: 650 }} aria-label="simple table" size="small">
-            <TableHead>
-              <TableRow>
-                <TableCell>Skill</TableCell>
-                <TableCell>&nbsp;</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {[
-                'Collaboration',
-                'Customer service',
-                'Health and wellbeing',
-                'Independence',
-                'Job coaching',
-                'Market research',
-                'Risk management'
-              ].map((skill) => (
-                <TableRow key={skill}>
-                  <TableCell scope="row">{skill}</TableCell>
-                  <TableCell scope="row" align="right">
-                    <Link href="#">Remove</Link>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TabPanel>
-      </Box>
+      <Tabs
+        tabItems={tabs.map((tab, i) => {
+          return i === tabIndex
+            ? {
+                ...tab,
+                content: (
+                  <Box sx={{ height: 'auto', width: '100%' }}>
+                    <DataGrid
+                      rows={rows}
+                      columns={columns}
+                      hideFooterPagination
+                      autoHeight
+                    />
+                  </Box>
+                )
+              }
+            : tab
+        })}
+        activeOnUrl
+      />
     </>
   )
 }
@@ -187,27 +73,30 @@ SkillsPage.getLayout = (page) => (
 )
 
 export async function getStaticPaths() {
-  const getIdFromHref = (href: string) =>
-    href.replace('/account/skills', '').split('/').at(-1)
-
   return {
     paths: tabs.map(({ href }) => ({
-      params: { tab: [getIdFromHref(href)] }
+      params: { tab: [createIdFromHref(href)] }
     })),
     fallback: false
   }
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const tab = context.params.tab || ['']
+  const tabIndex = tabs.map(({ href }) => createIdFromHref(href)).indexOf(tab[0])
+
   return {
-    props: {}
+    props: { tabIndex }
   }
 }
+
+const createIdFromHref = (href: string) =>
+  href.replace('/account/skills', '').split('/').at(-1)
 
 const tabs: TabItem[] = [
   {
     title: 'Your skills',
-    content: <SkillsList />,
+    content: null,
     href: '/account/skills'
   },
   {
@@ -217,7 +106,62 @@ const tabs: TabItem[] = [
   },
   {
     title: "Skills you'd like to develop",
-    content: <p>Content:skills develop</p>,
+    content: null,
     href: '/account/skills/skills-develop'
   }
+]
+
+const columns: GridColDef[] = [
+  {
+    field: 'name',
+    headerName: 'Skill name',
+    disableColumnMenu: true,
+    resizable: false,
+    flex: 1
+    // width: 200
+  },
+  {
+    field: 'firstName',
+    headerName: 'Skill level',
+    disableColumnMenu: true,
+    resizable: false,
+    renderCell: ({ formattedValue }) => formattedValue && <Chip label={formattedValue} />,
+    flex: 1
+  },
+  {
+    field: 'id',
+    headerName: '',
+    resizable: false,
+    disableColumnMenu: true,
+    sortable: false,
+    align: 'right',
+    width: 50,
+    renderCell: (cell) => {
+      return (
+        <IconButton
+          color="primary"
+          aria-label="delete"
+          component="label"
+          sx={{ color: 'text.primary' }}
+          onClick={() => {
+            console.log('formattedValue', cell.formattedValue)
+          }}
+        >
+          <Icon icon="circle-delete" />
+        </IconButton>
+      )
+    }
+  }
+]
+
+const rows = [
+  { id: 1, lastName: 'Snow', name: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', name: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', name: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', name: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', name: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', name: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', name: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', name: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', name: 'Roxie', firstName: 'Harvey', age: 65 }
 ]
