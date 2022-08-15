84e2a4db17d000bc4551c33d624dba6b8c7901e5
Nina Menezes <nina.menezes@example.com>
2022-08-15 20:42:29 +0100

    reload all data

---
+++ api/ellandi/registration/migrations/0057_loaddata_fixtures.py
@@ -0,0 +1,35 @@
+"""Load all data from fixtures for drop-down lists."""
+
+from django.core.management import call_command
+from django.db import migrations
+
+
+DROPDOWN_MODELS = {
+    "ContractType": "contracttypes",
+    "Country": "countries",
+    "Function": "functions",
+    "Grade": "grades",
+    "Language": "languages",
+    "LanguageSkillLevel": "languageskilllevels",
+    "Location": "locations",
+    "Organisation": "organisations",
+    "Profession": "professions",
+    "SkillLevel": "skilllevel",
+}
+
+
+def load_all_fixtures(apps, schema_editor):
+    for model_name, fixture_name in DROPDOWN_MODELS.items():
+        model = apps.get_model("registration", model_name)
+        model.objects.all().delete()
+        fixture_file = f"dropdown/{fixture_name}.json"
+        call_command("loaddata", fixture_file, app_label="registration", ignorenonexistent=True)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("registration", "0057_auto_20220815_1828"),
+    ]
+
+    operations = [migrations.RunPython(load_all_fixtures)]
