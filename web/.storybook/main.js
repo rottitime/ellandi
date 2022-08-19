2fbdd41be933c7d12aa8869136dcb2df7d75425f
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-08-19 16:53:37 +0100

    Storybook accessibility

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -11,7 +11,8 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    'storybook-addon-designs'
+    'storybook-addon-designs',
+    '@storybook/addon-a11y'
   ],
   framework: '@storybook/react',
   core: {
