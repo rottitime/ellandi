db1e0dc02db4170fb6b5a49d333929a44becc0e4
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-08-23 11:44:45 +0100

    Bug fix: Endpoint update with slash

--- web/service/me.ts
+++ web/service/me.ts
@@ -4,7 +4,7 @@ const { publicRuntimeConfig } = getConfig()
 const defaultError = 'Sorry, there is a problem with the service. Try again later.'
 
 export const fetchMe = async (token: string) => {
-  const res = await fetch(`${publicRuntimeConfig.apiUrl}/me`, {
+  const res = await fetch(`${publicRuntimeConfig.apiUrl}/me/`, {
     headers: { Authorization: `Token ${token}` }
   })
   if (res.ok) return res.json()
