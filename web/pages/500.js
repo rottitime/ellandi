3be1adb9d759143fff3578235dc1c2f5a9312920
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-08-26 17:15:42 +0100

    Custom error pages

---
+++ web/pages/500.js
@@ -0,0 +1,18 @@
+import CardLayout from '@/components/Layout/CardLayout/CardLayout'
+import { Link, Typography } from '@mui/material'
+
+export default function Custom500() {
+  return (
+    <CardLayout title="Sorry, there is a problem with the service">
+      <Typography gutterBottom>Try again later.</Typography>
+
+      <Typography gutterBottom>
+        If the problem persists,{' '}
+        <Link href="mailto:paul.harmer@cabinetoffice.gov.uk" target="_blank">
+          contact the Civil Service Skills and Learning team
+        </Link>
+        .
+      </Typography>
+    </CardLayout>
+  )
+}
