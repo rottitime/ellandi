aef8b25e3f1a79c0aabda6bec1dc1502f7f4f38d
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-09-12 15:11:53 +0100

    Temporary data

--- web/service/me.ts
+++ web/service/me.ts
@@ -20,42 +20,42 @@ export const fetchMeSuggestedSkills = async (token: string) => {
   const res = await fetch(`${publicRuntimeConfig.apiUrl}/me/skills-suggested/`, {
     headers: { Authorization: `Token ${token}` }
   })
-  if (res.ok) return res.json()
-  throw new Error(defaultError)
-
-  // return [
-  //   'Accessibility',
-  //   'Agile and Lean practices',
-  //   'Agile working',
-  //   'Agile working (content design)',
-  //   'Analysis',
-  //   'Analysis and synthesis',
-  //   'Analytical and problem-solving',
-  //   'Asset and configuration management',
-  //   'Availability and capacity management',
-  //   'Bridging the gap between the technical and non-technical',
-  //   'Business analysis',
-  //   'Business analysis (IT operations)',
-  //   'Business improvement process',
-  //   'Business modelling',
-  //   'Business process testing',
-  //   'Change management',
-  //   'Coding and scripting',
-  //   'Commercial management',
-  //   'Communicating analysis and insight',
-  //   'Communicating information',
-  //   'Communication skills',
-  //   'Communication skills (data)',
-  //   'Communication skills (security architect)',
-  //   'Community collaboration',
-  //   'Content concepts and prototyping',
-  //   'Continual service improvement',
-  //   'Continuity management',
-  //   'Customer service management',
-  //   'Cyber/security',
-  //   'Data analysis and synthesis',
-  //   'Data communication',
-  //   'Data development process',
-  //   'Data governance'
-  // ]
+  // if (res.ok) return res.json()
+  // throw new Error(defaultError)
+  //TODO: remove once api is ready
+  return [
+    'Accessibility',
+    'Agile and Lean practices',
+    'Agile working',
+    'Agile working (content design)',
+    'Analysis',
+    'Analysis and synthesis',
+    'Analytical and problem-solving',
+    'Asset and configuration management',
+    'Availability and capacity management',
+    'Bridging the gap between the technical and non-technical',
+    'Business analysis',
+    'Business analysis (IT operations)',
+    'Business improvement process',
+    'Business modelling',
+    'Business process testing',
+    'Change management',
+    'Coding and scripting',
+    'Commercial management',
+    'Communicating analysis and insight',
+    'Communicating information',
+    'Communication skills',
+    'Communication skills (data)',
+    'Communication skills (security architect)',
+    'Community collaboration',
+    'Content concepts and prototyping',
+    'Continual service improvement',
+    'Continuity management',
+    'Customer service management',
+    'Cyber/security',
+    'Data analysis and synthesis',
+    'Data communication',
+    'Data development process',
+    'Data governance'
+  ]
 }
