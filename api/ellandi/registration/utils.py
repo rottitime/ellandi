a565fdc204a1efebe4ffb1b99aa66d1b70116aa4
Nina Menezes <nina.menezes@example.com>
2022-09-13 21:52:11 +0100

    rename function

--- api/ellandi/registration/utils.py
+++ api/ellandi/registration/utils.py
@@ -1,9 +1,9 @@
 
 def reverse_dict_lists(initial_dict):
     output_dict = dict()
-    all_jobs_lists = list(initial_dict.values())
-    all_jobs = {job for job_list in all_jobs_lists for job in job_list}
-    for job in all_jobs:
-        relevant_skills = {skill for (skill, job_list) in initial_dict.items() if job in job_list}
-        output_dict[job] = list(relevant_skills)
+    all_lists = list(initial_dict.values())
+    all_values = {v for value in all_lists for v in value}
+    for v in all_values:
+        relevant_keys = {k for (k, value) in initial_dict.items() if v in value}
+        output_dict[v] = list(relevant_keys)
     return output_dict
