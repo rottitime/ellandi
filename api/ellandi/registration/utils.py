3d9299a68423227f0b7cf064e40b36c12f606afc
Nina Menezes <nina.menezes@example.com>
2022-09-14 13:49:04 +0100

    change function

--- api/ellandi/registration/utils.py
+++ api/ellandi/registration/utils.py
@@ -1,8 +1,6 @@
 def reverse_dict_lists(initial_dict):
-    output_dict = dict()
-    all_lists = list(initial_dict.values())
-    all_values = {v for value in all_lists for v in value}
-    for v in all_values:
-        relevant_keys = {k for (k, value) in initial_dict.items() if v in value}
-        output_dict[v] = list(relevant_keys)
-    return output_dict
+    skill_map = {}
+    pairs = ((job_title, key) for (key, value) in initial_dict.items() for job_title in value)
+    for job_title, skill in pairs:
+        skill_map.setdefault(job_title, []).append(skill)
+    return skill_map
