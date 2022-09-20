42b32db4e064e1eb3a78ef89f9c92aacc95db48d
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-09-20 16:22:09 +0100

    Tidy

--- web/service/account.ts
+++ web/service/account.ts
@@ -10,140 +10,111 @@ const { publicRuntimeConfig } = getConfig()
 import { defaultError } from '@/service/auth'
 
 export const deleteSkill = async (token: string, id: string) => {
-  const res: Response = await fetch(`${publicRuntimeConfig.apiUrl}/user-skills/${id}/`, {
-    method: 'DELETE',
-    headers: { Authorization: `Token ${token}` }
+  await api(token, `${publicRuntimeConfig.apiUrl}/user-skills/${id}/`, {
+    method: 'DELETE'
   })
-
-  if (res.ok) return { id }
-  throw new Error(defaultError)
+  return { id }
 }
 
 export const deleteLanguage = async (token: string, id: string) => {
-  const res: Response = await fetch(
-    `${publicRuntimeConfig.apiUrl}/user-languages/${id}/`,
-    {
-      method: 'DELETE',
-      headers: { Authorization: `Token ${token}` }
-    }
-  )
-  if (res.ok) return { id }
-  throw new Error(defaultError)
+  await api(token, `${publicRuntimeConfig.apiUrl}/user-languages/${id}/`, {
+    method: 'DELETE'
+  })
+  return { id }
 }
 
 export const deleteSkillDevelop = async (token: string, id: string) => {
-  const res: Response = await fetch(
-    `${publicRuntimeConfig.apiUrl}/user-skills-develop/${id}/`,
-    {
-      method: 'DELETE',
-      headers: { Authorization: `Token ${token}` }
-    }
-  )
-  if (res.ok) return { id }
-  throw new Error(defaultError)
+  await api(token, `${publicRuntimeConfig.apiUrl}/user-skills-develop/${id}/`, {
+    method: 'DELETE'
+  })
+  return { id }
 }
 
 export const addSkills = async (token: string, data: SkillType[]) => {
-  const res: Response = await fetch(`${publicRuntimeConfig.apiUrl}/me/skills/`, {
+  const res = await api(token, `${publicRuntimeConfig.apiUrl}/me/skills/`, {
     method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Token ${token}`
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
   })
-  if (res.ok) return await res.json()
-  throw new Error(defaultError)
+
+  return res.json()
 }
 
 export const editSkill = async (token: string, data: SkillType) => {
-  const res: Response = await fetch(`${publicRuntimeConfig.apiUrl}/me/skills/`, {
+  const res = await api(token, `${publicRuntimeConfig.apiUrl}/me/skills/`, {
     method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Token ${token}`
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
   })
-  if (res.ok) return await res.json()
-  throw new Error(defaultError)
+
+  return await res.json()
 }
 
 export const addLanguages = async (token: string, data: LanguageType[]) => {
-  const res: Response = await fetch(`${publicRuntimeConfig.apiUrl}/me/languages/`, {
+  const res = await api(token, `${publicRuntimeConfig.apiUrl}/me/languages/`, {
     method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Token ${token}`
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
   })
-  if (res.ok) return await res.json()
-  throw new Error(defaultError)
+
+  return res.json()
 }
 
 export const addSkillsToDevelop = async (token: string, data: SkillDevelopType[]) => {
-  const res: Response = await fetch(`${publicRuntimeConfig.apiUrl}/me/skills-develop/`, {
+  const res = await api(token, `${publicRuntimeConfig.apiUrl}/me/skills-develop/`, {
     method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Token ${token}`
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
   })
-  if (res.ok) return await res.json()
-  throw new Error(defaultError)
+
+  return res.json()
 }
 
 export const fetchTeam = async (token: string) => {
-  const res: Response = await fetch(`${publicRuntimeConfig.apiUrl}/me/direct-reports/`, {
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Token ${token}`
-    }
+  const res = await api(token, `${publicRuntimeConfig.apiUrl}/me/direct-reports/`, {
+    headers: { 'Content-Type': 'application/json' }
   })
-  if (res.ok) return await res.json()
-  throw new Error(defaultError)
+  return res.json()
 }
 
 export const addLearningOnTheJob = async (token: string, data: LearningAddType) => {
-  const res: Response = await fetch(
-    `${publicRuntimeConfig.apiUrl}/me/learning-on-the-job/`,
-    {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Token ${token}`
-      },
-      body: JSON.stringify(data)
-    }
-  )
-  if (res.ok) return await res.json()
-  throw new Error(defaultError)
+  const res = await api(token, `${publicRuntimeConfig.apiUrl}/me/learning-on-the-job/`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  })
+
+  return res.json()
 }
 
 export const addLearningSocial = async (token: string, data: LearningAddType) => {
-  const res: Response = await fetch(`${publicRuntimeConfig.apiUrl}/me/learning-social/`, {
+  const res = await api(token, `${publicRuntimeConfig.apiUrl}/me/learning-social/`, {
     method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Token ${token}`
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
   })
-  if (res.ok) return await res.json()
-  throw new Error(defaultError)
+
+  return res.json()
 }
 
 export const addLearningFormal = async (token: string, data: LearningAddFormalType) => {
-  const res: Response = await fetch(`${publicRuntimeConfig.apiUrl}/me/learning-formal/`, {
+  const res = await api(token, `${publicRuntimeConfig.apiUrl}/me/learning-formal/`, {
     method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Token ${token}`
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
   })
-  if (res.ok) return await res.json()
+  return res.json()
+}
+
+const api = async (
+  token: string,
+  input: RequestInfo | URL,
+  init?: RequestInit
+): Promise<Response> => {
+  const res: Response = await fetch(input, {
+    ...init,
+    headers: { ...init.headers, Authorization: `Token ${token}` }
+  })
+  if (res.ok) return res
   throw new Error(defaultError)
 }
