52a51e03ad675bf273c7f3fbb15ec9e4a4512562
Andreas Varotsis <andreas.varotsis@cabinetoffice.gov.uk>
2022-09-21 21:09:19 +0100

    TIDY

--- api/ellandi/registration/recommend.py
+++ api/ellandi/registration/recommend.py
@@ -8,21 +8,22 @@ import pandas as pd
 def make_skill_similarity_matrix(long_skill_df):
     """given a lit of users and their skills, returns a similarity matrix"""
 
-    #create a long datframe with all our users and skills
+    # create a long dataframe with all our users and skills
     sparse_df = long_skill_df.groupby(["user_id", "skill_name"])['rating'].sum().astype('Sparse[int]').unstack(
         fill_value=0)
-    #convert to sparse array to conserve memory
+    # convert to sparse array to conserve memory
     skills_sparse = scipy.sparse.csr_matrix(sparse_df.values)
 
-    #measure cosine similarity, and highliht most similar skills
+    # measure cosine similarity, and highlight most similar skills
     similarity_matrix = cosine_similarity(skills_sparse.T)
     return similarity_matrix
 
+
 def get_similar_skills(long_skill_df, skill_name, similarity_matrix, n=10):
-    """ given a long dataframe of user_id and skill_name and rating, returns a matrix of skill similarity based on common use"""
+    """ given a long dataframe of user_id and skill_name and rating, returns a matrix of skill similarity based on
+    common use """
 
-
-    #create a long datframe with all our users and skills
+    # create a long dataframe with all our users and skills
     sparse_df = long_skill_df.groupby(["user_id", "skill_name"])['rating'].sum().astype('Sparse[int]').unstack(
         fill_value=0)
 
@@ -30,12 +31,12 @@ def get_similar_skills(long_skill_df, skill_name, similarity_matrix, n=10):
     risk_index = np.where(sparse_df.columns == skill_name)
     similar_skills = cosine_sim[risk_index]
     sorted_skills = np.sort(similar_skills, axis=1)
-    top_skill = np.flip(sparse_df.columns[np.argsort(similar_skills, axis=1)][:,-(n+1):-1])
-    top_scores = np.flip(sorted_skills[:,-(n+1):-1])
+    top_skill = np.flip(sparse_df.columns[np.argsort(similar_skills, axis=1)][:, -(n + 1):-1])
+    top_scores = np.flip(sorted_skills[:, -(n + 1):-1])
     return top_skill, top_scores, cosine_sim
 
 
-def get_job_embeddings(job_titles, model_name = 'all-MiniLM-L6-v2'):
+def get_job_embeddings(job_titles, model_name='all-MiniLM-L6-v2'):
     """ given an array of job titles, returns them as embedding values to be stored"""
 
     model = SentenceTransformer(model_name)
@@ -45,7 +46,8 @@ def get_job_embeddings(job_titles, model_name = 'all-MiniLM-L6-v2'):
 
     return embedding_df
 
-def get_similar_embedding_jobs(job_title, job_embeddings, n=10,model_name = 'all-MiniLM-L6-v2'):
+
+def get_similar_embedding_jobs(job_title, job_embeddings, n=10, model_name='all-MiniLM-L6-v2'):
     """ given a series of job embeddings, a new title and optionally top N, returns most similar jobs"""
 
     model = SentenceTransformer(model_name)
@@ -56,9 +58,10 @@ def get_similar_embedding_jobs(job_title, job_embeddings, n=10,model_name = 'all
 
     return job_embeddings.index[jobs_by_dist][0:n]
 
-def return_similar_title_skills(job_title, user_skills, job_embeddings, n=10,model_name = 'all-MiniLM-L6-v2', missing_ratings=True):
-    """ takes a list of skills per job title, and returns most common skills for similar"""
 
+def return_similar_title_skills(job_title, user_skills, job_embeddings, n=10, model_name='all-MiniLM-L6-v2',
+                                missing_ratings=True):
+    """ takes a list of skills per job title, and returns most common skills for similar"""
 
     unique_job_titles = user_skills.drop_duplicates(subset=['clean_title']).dropna(axis=0)['clean_title'].to_numpy()
 
@@ -67,8 +70,8 @@ def return_similar_title_skills(job_title, user_skills, job_embeddings, n=10,mod
         long_skills = skills_to_dummy.copy().rename(columns={'vacancy_id': 'user_id'})
         long_skills['rating'] = 1
 
-    else :
-        skills_to_dummy = user_skills[["vacancy_id", "skill_name","rating"]]
+    else:
+        skills_to_dummy = user_skills[["vacancy_id", "skill_name", "rating"]]
         long_skills = skills_to_dummy.copy().rename(columns={'vacancy_id': 'user_id'})
 
     sparse_df = long_skills.groupby(["user_id", "skill_name"])['rating'].sum().astype('Sparse[int]').unstack(
@@ -79,7 +82,6 @@ def return_similar_title_skills(job_title, user_skills, job_embeddings, n=10,mod
 
     dist = np.linalg.norm(job_embedding - job_embeddings, axis=1)
     jobs_by_dist = np.argsort(dist)
-
 
     title_lookup = user_skills[["vacancy_id", "clean_title"]].drop_duplicates().set_index("vacancy_id")
 
@@ -94,10 +96,5 @@ def return_similar_title_skills(job_title, user_skills, job_embeddings, n=10,mod
     similar_skill_dist = comparison_df.corr()["distance"].sort_values()
 
     returned_skills = list(similar_skill_dist.iloc[0:n].index)
-    returned_jobs =   unique_job_titles[jobs_by_dist][0:n]
+    returned_jobs = unique_job_titles[jobs_by_dist][0:n]
     return returned_skills, returned_jobs
-
-
-
-
-
