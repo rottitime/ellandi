961add41830d037c6896c4d2bf454fffdfd73794
Antonio Lopes <i.ai@wdlog.com>
2022-09-22 14:02:01 +0100

    PR review

--- web/pages/_envz.tsx
+++ web/pages/_envz.tsx
@@ -4,7 +4,36 @@ import getConfig from 'next/config'
 const { publicRuntimeConfig } = getConfig()
 
 const Info = () => {
-  return <pre>{JSON.stringify(publicRuntimeConfig, null, 2)}</pre>
+  return (
+    <section
+      style={{
+        backgroundColor: '#fff',
+        fontSize: '16px',
+        lineHeight: '1.4em',
+        padding: '20px'
+      }}
+    >
+      <h1>Environment variables</h1>
+      <span
+        style={{
+          fontWeight: 'bold'
+        }}
+      >
+        NEXT_PUBLIC_API_URL
+      </span>
+      <p>{publicRuntimeConfig.apiUrl}</p>
+      <span
+        style={{
+          fontWeight: 'bold'
+        }}
+      >
+        NEXT_PUBLIC_GIT_SHA
+      </span>
+      <p>{publicRuntimeConfig.gitSHA}</p>
+      <h2>next.js publicRuntimeConfig</h2>
+      <pre>{JSON.stringify(publicRuntimeConfig, null, 2)}</pre>
+    </section>
+  )
 }
 
 export default Info
