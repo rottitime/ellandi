60b8caa196b10e72257a3949cd4d3e428cb2f38d
Ricky Parkinson <2026055+rickyp72@users.noreply.github.com>
2022-09-27 15:17:33 +0100

    Set CORS urls for local setup

--- api/ellandi/settings.py
+++ api/ellandi/settings.py
@@ -52,9 +52,9 @@ else:
 HOST_MAP = {
     "http://testserver": "http://testserver",
     "http://localhost:8000": "http://localhost:3000",
-    "http://localhost:8002": "http://localhost:8002",
+    "http://localhost:8000": "http://localhost:8080",
     "http://127.0.0.1:8000": "http://127.0.0.1:3000",
-    "http://127.0.0.1:8002": "http://127.0.0.1:8002",
+    "http://127.0.0.1:8000": "http://127.0.0.1:8080",
     "https://ellandi-api-sandbox.london.cloudapps.digital": "https://ellandi-web-sandbox.london.cloudapps.digital",
     "https://ellandi-api-temp.london.cloudapps.digital": "https://ellandi-web-temp.london.cloudapps.digital",
     "https://ellandi-api-demo.london.cloudapps.digital": "https://ellandi-web-demo.london.cloudapps.digital",
