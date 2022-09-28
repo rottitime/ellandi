a7dc3b04105a7e4ded81aac8b123a9f62bde313a
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-09-28 10:31:57 +0100

    Unit test for Registration steps page
    No token redirect
    
    Registration test fix

---
+++ web/__tests__/register-step.test.tsx
@@ -0,0 +1,70 @@
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import RegisterStepPage from '@/pages/register/step/[step]'
+import fetchMock from 'jest-fetch-mock'
+import { bugfixForTimeout, mockMe, renderWithProviders } from '@/lib/test-utils'
+
+jest.mock('@/components/Form/Register/RegisterDetailsForm/RegisterDetailsForm', () => ({
+  __esModule: true,
+  namedExport: jest.fn(),
+  default: jest.fn(() => <>mock-form</>)
+}))
+
+const mockReplace = jest.fn()
+jest.mock('next/router', () => ({
+  ...jest.requireActual('next/router'),
+  useRouter() {
+    return {
+      replace: mockReplace
+    }
+  }
+}))
+
+describe('Page: Registration steps', () => {
+  afterEach(() => {
+    fetchMock.resetMocks()
+    jest.resetAllMocks()
+  })
+
+  it('valid user with token', async () => {
+    sessionStorage.setItem('token', 'my-token')
+    fetchMock.mockResponse(JSON.stringify(mockMe), { status: 200 })
+
+    renderWithProviders(
+      <RegisterStepPage
+        stepInt={0}
+        nextUrl=""
+        title="test"
+        backUrl=""
+        progress={0}
+        skip={true}
+      />
+    )
+    await bugfixForTimeout()
+
+    await waitFor(async () => expect(mockReplace).not.toHaveBeenCalled())
+    expect(screen.getByText('mock-form')).toBeInTheDocument()
+  })
+
+  it('invalid user, redirects back to signup', async () => {
+    fetchMock.mockResponses([JSON.stringify(mockMe), { status: 200 }])
+
+    renderWithProviders(
+      <RegisterStepPage
+        stepInt={0}
+        nextUrl=""
+        title="test"
+        backUrl=""
+        progress={0}
+        skip={true}
+      />
+    )
+
+    await waitFor(async () => {
+      expect(mockReplace).toHaveBeenCalledWith({
+        pathname: '/register',
+        query: { ecode: 1 }
+      })
+    })
+  })
+})
