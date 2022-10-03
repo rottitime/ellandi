896aff051156195e6656d82c746f1a24abb92c59
Ed Singleton <singletoned@gmail.com>
2022-10-03 08:57:38 +0100

    Add GIT_SHA to view

--- api/ellandi/debug.py
+++ api/ellandi/debug.py
@@ -7,8 +7,7 @@ from rest_framework.response import Response
 @decorators.api_view(["GET"])
 @decorators.permission_classes((permissions.IsAdminUser,))
 def debug_view(request):
-    vars_of_interest = ["DEBUG", "EMAIL_BACKEND_TYPE", "ALLOW_EXAMPLE_EMAILS"]
-    # TODO - add latest Git SHA
+    vars_of_interest = ["DEBUG", "EMAIL_BACKEND_TYPE", "ALLOW_EXAMPLE_EMAILS", "GIT_SHA"]
     debug_vars = {}
     for env_var in vars_of_interest:
         debug_vars[env_var] = os.getenv(env_var)
