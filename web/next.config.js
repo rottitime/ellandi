68459afc4bdb02c6d2a40de10c74a9430449d0f7
Ed Singleton <singletoned@gmail.com>
2022-10-03 08:56:05 +0100

    Change NEXT_PUBLIC_GIT_SHA to GIT_SHA

--- web/next.config.js
+++ web/next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   trailingSlash: true,
   publicRuntimeConfig: {
     apiUrl: process.env.NEXT_PUBLIC_API_URL,
-    gitSHA: process.env.NEXT_PUBLIC_GIT_SHA || 'development',
+    gitSHA: process.env.GIT_SHA || 'development',
     urls: {
       landingSignin: '/account/',
       signin: '/signin/'
