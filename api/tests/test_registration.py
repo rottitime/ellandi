9925d7b1bd354398b1c9d6fadf9b5db8d75cfeba
Nina Menezes <nina.menezes@example.com>
2022-10-03 09:42:41 +0100

    update tests

--- api/tests/test_registration.py
+++ api/tests/test_registration.py
@@ -134,7 +134,7 @@ def test_me_patch(client, user_id):
 
 @utils.with_logged_in_client
 def test_me_patch_lm_email_validation(client, user_id):
-    endpoint = "/me/"
+    endpoint = "/api/me/"
     response = client.patch(endpoint, json={"line_manager_email": "Jane@example.com"})
     assert response.status_code == status.HTTP_400_BAD_REQUEST, response.status_code
     assert response.json()["detail"] == "Line manager email cannot be the same as user email", response.json()
