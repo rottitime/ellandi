1046b02c34bc2bdbb4127e3e88b4946fe0a97992
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-10-07 09:44:10 +0100

    Profile icon updates

--- web/components/Icon/Icons/Mail.tsx
+++ web/components/Icon/Icons/Mail.tsx
@@ -14,17 +14,23 @@ const SvgComponent = ({
     width="1em"
     height="1em"
     fill="none"
-    viewBox="0 0 25 25"
     xmlns="http://www.w3.org/2000/svg"
     aria-labelledby={titleId}
+    viewBox="0 0 25 24"
     {...props}
   >
     {title ? <title id={titleId}>{title}</title> : null}
-    <circle cx={12.5} cy={12.5} r={12.5} fill="currentColor" />
-    <path
-      d="M19.3 6H5.7c-.935 0-1.692.731-1.692 1.625L4 17.375C4 18.269 4.765 19 5.7 19h13.6c.935 0 1.7-.731 1.7-1.625v-9.75C21 6.731 20.235 6 19.3 6Zm-.34 3.453-6.01 3.591a.894.894 0 0 1-.9 0l-6.01-3.59a.689.689 0 0 1-.34-.586c0-.544.62-.87 1.105-.585l5.695 3.405 5.695-3.405c.485-.284 1.105.04 1.105.585a.689.689 0 0 1-.34.585Z"
-      fill="#fff"
-    />
+    <g clipPath="url(#a)">
+      <path
+        d="M20.5 4h-16c-1.1 0-1.99.9-1.99 2L2.5 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2Zm-.4 4.25-7.07 4.42c-.32.2-.74.2-1.06 0L4.9 8.25a.85.85 0 1 1 .9-1.44L12.5 11l6.7-4.19a.85.85 0 1 1 .9 1.44Z"
+        fill="currentColor"
+      />
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path fill="#fff" transform="translate(.5)" d="M0 0h24v24H0z" />
+      </clipPath>
+    </defs>
   </svg>
 )
 
