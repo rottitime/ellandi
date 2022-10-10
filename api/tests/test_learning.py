c440892ae465b7b6b1605a10958da832ed3e71ff
Nina Menezes <nina.menezes@example.com>
2022-10-10 20:05:58 +0100

    update learning tests

--- api/tests/test_learning.py
+++ api/tests/test_learning.py
@@ -45,12 +45,6 @@ def test_me_learning_formal(client, user_id):
 def test_me_learning_patch_get_delete(client, user_id):
     data = [
         {
-            "name": "OTJ learning",
-            "duration_minutes": 666,
-            "date_completed": "2022-09-20",
-            "learning_type": "On the job",
-        },
-        {
             "name": "Did some on the job learning",
             "duration_minutes": 32767,
             "date_completed": "2022-09-21",
@@ -78,19 +72,29 @@ def test_me_learning_patch_get_delete(client, user_id):
 
     for i, learning_type in enumerate(("On the job", "Social", "Formal")):
         response = client.get(f"/api/me/learnings/?learning_type={learning_type}")
-
         result = response.json()
         for key, value in data[i].items():
-            assert result[0][key] == value
-            assert all_result[i][key] == value
+            assert result[0][key] == value, result
+            assert all_result[i][key] == value, all_result
 
-    response = client.get(f"/api/me/learnings/?learning_type=On the job&?sortfield=name")
+    more_data = [
+        {
+            "name": "OTJ learning",
+            "duration_minutes": 666,
+            "date_completed": "2022-09-20",
+            "learning_type": "On the job",
+        },
+    ]
+    response = client.patch("/api/me/learnings/", json=more_data)
+    assert response.status_code == status.HTTP_200_OK, response
+
+    response = client.get(f"/api/me/learnings/?learning_type=On the job&sortfield=name")
     result = response.json()
     assert len(result) == 2
-    assert result[0]["name"] == "Did some on the job learning"
+    assert result[0]["name"] == "Did some on the job learning", result
     assert result[1]["learning_type"] == "On the job"
 
-    response = client.get(f"/api/me/learnings/?learning_type=sortfield=-duration_minutes")
+    response = client.get(f"/api/me/learnings/?sortfield=-duration_minutes")
     result = response.json()
     assert len(result) == 4
     assert result[3]["duration_minutes"] == 666
