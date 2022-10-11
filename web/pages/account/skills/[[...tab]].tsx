51fae1c53ed5ed0ab9ee5cf5332500ae05335d3c
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-10-11 11:20:50 +0100

    Tidy

--- web/pages/account/skills/[[...tab]].tsx
+++ web/pages/account/skills/[[...tab]].tsx
@@ -1,10 +1,6 @@
 import AccountLayout from '@/components/Layout/AccountLayout/AccountLayout'
 import { Typography } from '@mui/material'
 import Button from '@/components/UI/Button/Button'
-import useAuth from '@/hooks/useAuth'
-import { useQuery } from 'react-query'
-import { Query, RegisterUserResponse } from '@/service/api'
-import { fetchMe } from '@/service/me'
 import Headline from '@/components/Account/Headline/Headline'
 import SkillsList from '@/components/Account/SkillsList/SkillsList'
 import LanguageList from '@/components/Account/LanguageList/LanguageList'
@@ -13,8 +9,6 @@ import RoutedTabs, { RoutedTabItem } from '@/components/UI/Tabs/RoutedTabs'
 import { useRouter } from 'next/router'
 
 const SkillsPage = () => {
-  const { authFetch } = useAuth()
-  const { data } = useQuery<RegisterUserResponse>(Query.Me, () => authFetch(fetchMe))
   const router = useRouter()
 
   return (
