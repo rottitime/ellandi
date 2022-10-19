375bd69e7c3623bdff1dd115329dc3a2e9e138f6
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-10-19 12:11:59 +0100

    Environment variable setup

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   trailingSlash: true,
   publicRuntimeConfig: {
+    enableEmailVerify:
+      process.env.NEXT_PUBLIC_ENABLE_EMAIL_VERIFY.toLowerCase() === 'true',
     apiUrl: process.env.NEXT_PUBLIC_API_URL,
     gitSHA: process.env.GIT_SHA || 'development',
     urls: {
