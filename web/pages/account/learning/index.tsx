522feab92df366380275a89a385a1c8f476210cf
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-10-24 09:53:07 +0100

    Content

--- web/pages/account/learning/index.tsx
+++ web/pages/account/learning/index.tsx
@@ -135,7 +135,7 @@ const LearningPage = () => {
                 Learning goal
               </Typography>
               <GraphDescription variant="body2" gutterBottom>
-                You should aim to complete 10 days learning each financial year
+                You should aim to complete 10 days learning each year
               </GraphDescription>
 
               <LearningGoalBar />
