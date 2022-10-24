f9ad351820b6190c996ea4aa5a8525cdfe2ce119
Andreas Varotsis <andreas.varotsis@cabinetoffice.gov.uk>
2022-10-24 08:56:14 +0100

    New recommendation app
    Recommendation app broken out into separate docker container, and new functionality to read from db

--- recommender/recommend/models.py
+++ recommender/recommend/models.py
@@ -3,21 +3,42 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String, Float, DateTime, LargeBinary
 from sqlalchemy import create_engine
 from settings_base import DB_URL
+import pandas as pd
+
 
 
 class TitleEmbeddingArray(Base):
     __tablename__ = "tblTitleEmbeddings"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    jobTitle = Column(Integer)
+    jobTitle = Column(String)
     variable = Column(Integer)
     value = Column(Float)
-
 
 class SkillSimilarityArray(Base):
     __tablename__ = "tblSkillArray"
     id = Column(Integer, primary_key=True, autoincrement=True)
     createdAt = Column(DateTime)
     array = Column(LargeBinary)
+
+class UserSkills(Base):
+    __tablename__ = "registration_userskill"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    createdAt = Column(DateTime)
+    array = Column(LargeBinary)
+
+class SkillRecommendations(Base):
+    __tablename__ = "tblSkillRecommendations"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    createdAt = Column(DateTime)
+    currentSkill = Column(String)
+    recommendedSkill = Column(String)
+
+class TitleRecommendations(Base):
+    __tablename__ = "tblTitleRecommendations"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    createdAt = Column(DateTime)
+    jobTitle = Column(String)
+    recommendedSkill = Column(String)
 
 def create_db_objects():
     """ declares and creates all our db objects"""
@@ -27,3 +48,48 @@ def create_db_objects():
     engine = create_engine(DB_URL)
     #create all dbs
     Base.metadata.create_all(engine)
+
+
+def return_db_user_skills():
+    """ returns a pandas dataframe with all user skills"""
+
+    query = """
+    SELECT user_id,
+     name,
+     FROM registration_userskill
+    """
+
+    engine = create_engine(DB_URL)
+
+    all_user_skills = pd.read_sql(query, engine)
+    all_user_skills['rating'] = 1
+    all_user_skills.columns = ["user_id", "skill_name", "rating"]
+
+    return all_user_skills
+
+def return_db_user_title_skills():
+    """ returns a pandas dataframe with all user skills"""
+
+    query = """
+    SELECT id,
+    job_title,
+    registration_userskill.name
+    FROM registration_user
+    INNER JOIN registration_userskill
+    ON registration_user.id = registration_userskill.user_id
+    """
+
+    engine = create_engine(DB_URL)
+
+    all_user_skills = pd.read_sql(query, engine)
+    all_user_skills['rating'] = 1
+    all_user_skills.columns = ["user_id", "job_title","skill_name", "rating"]
+
+    return all_user_skills
+
+def return_nlp_user_skills():
+    """ returns a pandas dataframe of nlp based user skills"""
+
+    nlp_skill_df = pd.read_json("nlp_generated_skills.json")[["user_id", "skill_name", "rating"]]
+    return nlp_skill_df
+
--- recommender/recommend/recommend.py
+++ recommender/recommend/recommend.py
@@ -9,32 +9,18 @@ from sqlalchemy import create_engine
 from datetime import datetime
 import pickle
 from sqlalchemy.orm import sessionmaker
+import numpy as np
 
-from models import TitleEmbeddingArray, SkillSimilarityArray, create_db_objects
+from models import TitleEmbeddingArray, SkillSimilarityArray, create_db_objects, return_db_user_skills, return_nlp_user_skills, return_db_user_title_skills
 from settings_base import DB_URL
 
 
-
 def make_skill_similarity_matrix():
-    nlp_skill_df = pd.read_json("nlp_generated_skills.json")[["user_id", "skill_name", "rating"]]
-    qs = models.UserSkill.objects.all().values_list("user__id", "id", "name", "user__job_title")
+    nlp_skill_df = return_nlp_user_skills()
+    db_user_skills = return_db_user_skills()
 
-    if len(qs) > 0:
-
-        df = pd.DataFrame.from_records(qs).rename(
-            columns={0: "user_id", 1: "skill_id", 2: "skill_name", 3: "job_title"}
-        )
-
-        long_df = df[["user_id", "skill_name"]].copy()
-
-        # currently assuming all users have similar competence (1), though we can iterate on this later
-        long_df["rating"] = 1
-        # todo - fix to uuid
-        long_df["user_id"] = long_df["user_id"].astype("string")
-        long_df["skill_name"] = long_df["skill_name"].astype("string")
-        long_df["rating"] = long_df["rating"].astype("int")
-
-        skill_compare_df = pd.concat([long_df, nlp_skill_df]).reset_index(drop=True)
+    if len(db_user_skills) > 0:
+        skill_compare_df = pd.concat([nlp_skill_df, db_user_skills]).reset_index(drop=True)
 
     else:
         skill_compare_df = nlp_skill_df.copy()
@@ -55,18 +41,12 @@ def create_job_embedding_matrix():
 
     model_name = "all-MiniLM-L6-v2"
 
-    qs = models.UserSkill.objects.all().values_list("user__id", "user__job_title")
-
-    nlp_jobs_df = pd.read_json("nlp_generated_skills.json")[["user_id", "job_title"]]
+    qs = return_db_user_title_skills()[["user_id", "job_title"]]
+    nlp_jobs_df = return_nlp_user_skills()[["user_id", "job_title"]]
 
     if len(qs) > 0:
 
-        user_df = pd.DataFrame.from_records(qs).rename(columns={0: "user_id", 1: "job_title"})
-        # todo - fix to uuid
-        user_df["user_id"] = user_df["user_id"].astype("string")
-        user_df["job_title"] = user_df["job_title"].astype("string")
-
-        df = pd.concat([user_df, nlp_jobs_df]).reset_index(drop=True)
+        df = pd.concat([qs, nlp_jobs_df]).reset_index(drop=True)
 
     else:
         df = nlp_jobs_df.copy()
@@ -106,4 +86,201 @@ def store_matrices(skill_similarity_matrix, job_embeddings_matrix):
 
     session.add(current_array)
     session.commit()
+
+def return_similar_title_skills(job_title, user_skills, job_embeddings):
+    """Given a job title, a list of all user skills, and previously generated job embeddings, returns likely skills
+
+    The number of skills returned is a hard coded value (default to 10), as is the model name.
+    The model assumes no skill ratings are provided (and defaults to each having a score of 1)
+    """
+
+    skill_count = 60
+    model_name = "all-MiniLM-L6-v2"
+    missing_ratings = False
+
+    if job_title not in job_embeddings.index.to_list():
+        user_skills = user_skills[user_skills["job_title"] != job_title].copy()
+
+    if missing_ratings:
+        skills_to_dummy = user_skills[["user_id", "skill_name"]]
+        long_skills = skills_to_dummy.copy().rename(columns={"user_id": "user_id"})
+        long_skills["rating"] = 1
+
+    else:
+        skills_to_dummy = user_skills[["user_id", "skill_name", "rating"]]
+        long_skills = skills_to_dummy.copy().rename(columns={"user_id": "user_id"})
+
+    if job_title is None:
+        top_skills = (
+            long_skills.groupby("skill_name")["rating"]
+            .sum()
+            .sort_values(ascending=False)
+            .iloc[0:skill_count]
+            .index.to_list()
+        )
+        return top_skills, None
+
+    sparse_df = (
+        long_skills.groupby(["user_id", "skill_name"])["rating"].sum().astype("Sparse[int]").unstack(fill_value=0)
+    )
+
+    model = SentenceTransformer(model_name)
+    embeddings = model.encode(job_title)
+    job_embedding = embeddings.reshape((1, embeddings.shape[0]))
+
+    dist = np.linalg.norm(job_embedding - job_embeddings, axis=1)
+
+    title_lookup = user_skills[["user_id", "job_title"]].drop_duplicates().set_index("user_id")
+
+    dist_df = pd.DataFrame(columns=["distance", "job_title"])
+
+    dist_df["distance"] = dist
+    dist_df["job_title"] = job_embeddings.index.to_list()
+
+    distance_title_df = (
+        title_lookup.reset_index()
+        .merge(dist_df, right_on="job_title", left_on="job_title", how="left")
+        .set_index("user_id")
+    )
+    comparison_df = (
+        distance_title_df.merge(sparse_df, left_index=True, right_index=True)
+        .fillna(0)
+        .copy()
+        .drop(columns=["job_title", "job_title"])
+    )
+    similar_skill_dist = comparison_df.corr()["distance"].sort_values()
+
+    returned_skills = list(similar_skill_dist.iloc[0:skill_count].index)
+    return returned_skills
+
+
+def get_similar_skills(long_skill_df, skill_name, similarity_matrix):
+    """Given a pandas dataframe of user_id, skill_name, and rating, returns a matrix of skill similarity based on
+    other users the data
+
+    skill_return_count is a hard coded value (default to 10) to specify number of skill returned."""
+
+    skill_return_count = 10
+
+    sparse_df = (
+        long_skill_df.groupby(["user_id", "skill_name"])["rating"].sum().astype("Sparse[int]").unstack(fill_value=0)
+    )
+
+    cosine_sim = similarity_matrix
+    risk_index = np.where(sparse_df.columns == skill_name)
+    similar_skills = cosine_sim[risk_index]
+    sorted_skills = np.sort(similar_skills, axis=1)
+    top_skill = np.flip(sparse_df.columns[np.argsort(similar_skills, axis=1)][:, -(skill_return_count + 1) : -1])
+    return top_skill
+
+def return_similar_title_skills(job_title, user_skills, job_embeddings):
+    """Given a job title, a list of all user skills, and previously generated job embeddings, returns likely skills
+
+    The number of skills returned is a hard coded value (default to 10), as is the model name.
+    The model assumes no skill ratings are provided (and defaults to each having a score of 1)
+    """
+
+    skill_count = 60
+    model_name = "all-MiniLM-L6-v2"
+    missing_ratings = False
+
+    if job_title not in job_embeddings.index.to_list():
+        user_skills = user_skills[user_skills["job_title"] != job_title].copy()
+
+    if missing_ratings:
+        skills_to_dummy = user_skills[["user_id", "skill_name"]]
+        long_skills = skills_to_dummy.copy().rename(columns={"user_id": "user_id"})
+        long_skills["rating"] = 1
+
+    else:
+        skills_to_dummy = user_skills[["user_id", "skill_name", "rating"]]
+        long_skills = skills_to_dummy.copy().rename(columns={"user_id": "user_id"})
+
+    if job_title is None:
+        top_skills = (
+            long_skills.groupby("skill_name")["rating"]
+            .sum()
+            .sort_values(ascending=False)
+            .iloc[0:skill_count]
+            .index.to_list()
+        )
+        return top_skills, None
+
+    sparse_df = (
+        long_skills.groupby(["user_id", "skill_name"])["rating"].sum().astype("Sparse[int]").unstack(fill_value=0)
+    )
+
+    model = SentenceTransformer(model_name)
+    embeddings = model.encode(job_title)
+    job_embedding = embeddings.reshape((1, embeddings.shape[0]))
+
+    dist = np.linalg.norm(job_embedding - job_embeddings, axis=1)
+
+    title_lookup = user_skills[["user_id", "job_title"]].drop_duplicates().set_index("user_id")
+
+    dist_df = pd.DataFrame(columns=["distance", "job_title"])
+
+    dist_df["distance"] = dist
+    dist_df["job_title"] = job_embeddings.index.to_list()
+
+    distance_title_df = (
+        title_lookup.reset_index()
+        .merge(dist_df, right_on="job_title", left_on="job_title", how="left")
+        .set_index("user_id")
+    )
+    comparison_df = (
+        distance_title_df.merge(sparse_df, left_index=True, right_index=True)
+        .fillna(0)
+        .copy()
+        .drop(columns=["job_title", "job_title"])
+    )
+    similar_skill_dist = comparison_df.corr()["distance"].sort_values()
+
+    returned_skills = list(similar_skill_dist.iloc[0:skill_count].index)
+    return returned_skills
+
+
+def store_skill_recommendations(skill_similarity_matrix):
+    """ creates recommended skills for all skills, and stores in db"""
+
+    engine = create_engine(DB_URL, echo=True)
+
+    nlp_skill_df = return_nlp_user_skills()
+    db_user_skills = return_db_user_skills()
+    skill_compare_df = pd.concat([nlp_skill_df, db_user_skills]).reset_index(drop=True)
+
+    for unique_skill in skill_compare_df['skill_name'].unique():
+        recommended_skills = get_similar_skills(skill_compare_df, unique_skill, skill_similarity_matrix)
+
+        recommended_skills = pd.DataFrame(data=recommended_skills,columns=['recommendedSkill'])
+        recommended_skills.createdAt = datetime.now()
+        recommended_skills.currentSkill = unique_skill
+
+        recommended_skills.to_sql('tblSkillRecommendations',engine,if_exists='append')
+
+
+def store_title_recommendations(job_embeddings):
+    """ creates recommended skills for all job_titles, and stores in db"""
+
+    engine = create_engine(DB_URL, echo=True)
+
+    qs = return_db_user_title_skills()[["user_id", "job_title"]]
+    nlp_jobs_df = return_nlp_user_skills()[["user_id", "job_title"]]
+
+    user_skills = return_db_user_title_skills()
+
+    title_compare_df = pd.concat([qs, nlp_jobs_df]).reset_index(drop=True)
+
+    for job_title in title_compare_df['job_title'].unique():
+        recommended_skills = return_similar_title_skills(job_title, user_skills, job_embeddings)
+
+        recommended_skills = pd.DataFrame(data=recommended_skills, columns=['recommendedSkill'])
+        recommended_skills.createdAt = datetime.now()
+        recommended_skills.jobTitle = job_title
+
+        recommended_skills.to_sql('tblTitleRecommendations', engine, if_exists='append')
+
+
+
+
 
