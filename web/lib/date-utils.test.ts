31762546eece574143a51ede867d3696d9e810cb
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-10-25 14:02:29 +0100

    deprecated functions

--- web/lib/date-utils.test.ts
+++ web/lib/date-utils.test.ts
@@ -1,30 +1,8 @@
 import {
-  splitDays,
-  combineDaysMinutesHoursToDays,
   splitMinutes,
   combineDaysMinutesHoursToMinutes,
   isBetweenBusinessDates
 } from './date-utils'
-
-describe('splitDays()', () => {
-  it('returns correct value', () => {
-    expect(splitDays(0)).toMatchObject({ days: 0, hours: 0, minutes: 0 })
-    expect(splitDays(2.5)).toMatchObject({ days: 2, hours: 12, minutes: 0 })
-    expect(splitDays(2.6)).toMatchObject({ days: 2, hours: 14, minutes: 24 })
-    expect(splitDays(2.08)).toMatchObject({ days: 2, hours: 1, minutes: 55 })
-    expect(splitDays(2.04)).toMatchObject({ days: 2, hours: 0, minutes: 57 })
-  })
-})
-
-describe('combineDaysMinutesHoursToDays()', () => {
-  it('returns correct value', () => {
-    expect(combineDaysMinutesHoursToDays(0, 0, 0)).toEqual(0)
-    expect(combineDaysMinutesHoursToDays(2, 12, 0)).toEqual(2.5)
-    expect(combineDaysMinutesHoursToDays(2, 14, 24)).toEqual(2.6)
-    expect(combineDaysMinutesHoursToDays(2, 1, 55)).toEqual(2.08)
-    expect(combineDaysMinutesHoursToDays(2, 0, 57)).toEqual(2.04)
-  })
-})
 
 describe('splitMinutes()', () => {
   it('returns correct value', () => {
--- web/lib/date-utils.ts
+++ web/lib/date-utils.ts
@@ -2,32 +2,6 @@ import dayjs from 'dayjs'
 import isBetween from 'dayjs/plugin/isBetween'
 dayjs.extend(isBetween)
 
-export const splitDays = (
-  totalDays: number
-): { days: number; hours: number; minutes: number } => {
-  const days = Math.floor(totalDays)
-  const remainder = totalDays - days
-  const remainderHours = remainder * 24
-
-  return {
-    days,
-    hours: Math.floor(remainderHours),
-    minutes: Math.floor((remainderHours % 1) * 60)
-  }
-}
-
-export const combineDaysMinutesHoursToDays = (
-  days: number,
-  hours: number,
-  minutes: number
-): number => {
-  const hoursAsDays = hours / 24
-  const minutesAsDays = minutes / 1440
-  const total = days + hoursAsDays + minutesAsDays
-
-  return Math.round(total * 100) / 100
-}
-
 export const splitMinutes = (
   totalMinutes: number
 ): { days: number; hours: number; minutes: number } => ({
