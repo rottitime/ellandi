6f1730cc2e5f012e9833d00b4989f5f0e736cda0
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-10-26 09:04:51 +0100

    Update date-utils.ts

--- web/lib/date-utils.ts
+++ web/lib/date-utils.ts
@@ -5,40 +5,11 @@ dayjs.extend(isBetween)
 export const splitMinutes = (
   totalMinutes: number,
   minutesPerDay = 1440 //24 hours per day
-): { days: number; hours: number; minutes: number } => {
-  console.log('before', { totalMinutes, minutesPerDay })
-
-  const returnValue = {
-    days: Math.floor(totalMinutes / minutesPerDay),
-    hours: Math.floor((totalMinutes % minutesPerDay) / 60),
-    // hours: Math.floor((totalMinutes / 60) % 7.4),
-    minutes: (totalMinutes % minutesPerDay) % 60
-    // minutes: totalMinutes % 60 //value of 24
-  }
-  console.log(
-    'after',
-    { totalMinutes, minutesPerDay, returnValue },
-    (totalMinutes % minutesPerDay) / 60
-  )
-  return returnValue
-}
-
-// export const splitMinutes = (
-//   totalMinutes: number,
-//   hoursPerDay = 24
-// ): { days: number; hours: number; minutes: number } => {
-//   const minutesValue = totalMinutes % 60
-//   // const minutesValue = 14.399999999999977
-
-//   return {
-//     days: Math.floor(totalMinutes / hoursPerDay / 60),
-//     hours: Math.floor((totalMinutes / 60) % hoursPerDay),
-//     minutes: totalMinutes % 60
-//   }
-// }
-
-export const minutesToHours = (minutes: number): number =>
-  Math.round((minutes / 60) * 100) / 100
+): { days: number; hours: number; minutes: number } => ({
+  days: Math.floor(totalMinutes / minutesPerDay),
+  hours: Math.floor((totalMinutes % minutesPerDay) / 60),
+  minutes: (totalMinutes % minutesPerDay) % 60
+})
 
 export const combineDaysMinutesHoursToMinutes = (
   days: number,
