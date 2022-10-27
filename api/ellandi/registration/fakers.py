1786100c3b6c44b98741454649f216cef615e424
Ed Singleton <singletoned@gmail.com>
2022-10-27 13:33:50 +0100

    Add a faker for courses

---
+++ api/ellandi/registration/fakers.py
@@ -0,0 +1,21 @@
+import random
+
+import faker
+
+from . import models
+
+fake = faker.Faker()
+
+
+def make_fake_course():
+    data = {
+        'title': fake.sentence(),
+        'short_description': fake.paragraph(),
+        'long_description': "\n".join(fake.paragraphs()),
+        'status': random.choice(models.Course.Status.values),
+        'cost_pounds': fake.pyint(),
+        'duration_minutes': fake.pyint(),
+        'private': random.choice((False, False, True)),
+        'course_type': random.choice(models.Course.CourseType.values),
+    }
+    return data
