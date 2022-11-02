d4ee3fa88b2e60595ec2fe35c6244966bb250c04
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-11-02 12:45:49 +0000

    Simple string function for data lists

--- web/lib/data-utils.ts
+++ web/lib/data-utils.ts
@@ -1,5 +1,9 @@
+import { GenericDataList } from '@/service/types'
+
 export const sortWithOrder = (a: number, b: number): number => {
   if (a < b) return -1
   if (a > b) return 1
   return 0
 }
+
+export const asStringList = (data: GenericDataList[]) => data.map(({ name }) => name)
