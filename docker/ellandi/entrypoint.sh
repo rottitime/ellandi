837014e76c5a35286824292f097d5e9a7ea0ea76
Ricky Parkinson <2026055+rickyp72@users.noreply.github.com>
2022-11-03 09:57:08 +0000

    Remove old ellandi docker files

--- docker/ellandi/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-set -o nounset
-
-wait-for-db --mode postgres --connection-string $DATABASE_URL --timeout 60 --sql-query "select 1;"
-
-exec "$@"
--- docker/ellandi/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-set -o nounset
-
-python manage.py migrate --noinput
-
-watchmedo auto-restart --directory=./  --pattern='*.py' --recursive -- python manage.py runserver 0.0.0.0:8002 --noreload
