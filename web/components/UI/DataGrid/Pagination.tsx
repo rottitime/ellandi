234b4e32efd9020fe2090e2e1d79196177c9c7c0
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-11-07 08:51:19 +0000

    Hide pagination for 1 row

--- web/components/UI/DataGrid/Pagination.tsx
+++ web/components/UI/DataGrid/Pagination.tsx
@@ -11,13 +11,13 @@ const Pagination = () => {
   const page = useGridSelector(apiRef, gridPageSelector)
   const pageCount = useGridSelector(apiRef, gridPageCountSelector)
 
-  return (
+  return pageCount > 1 ? (
     <MuiPagination
       count={pageCount}
       page={page + 1}
       onChange={(_, value) => apiRef.current.setPage(value - 1)}
     />
-  )
+  ) : null
 }
 
 export default Pagination
