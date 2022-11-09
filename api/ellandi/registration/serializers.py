723ba8040f89a00eb18e00fd74817cc4d134fabb
Nina Menezes <nina.menezes@example.com>
2022-11-09 16:14:35 +0000

    blacken

--- api/ellandi/registration/serializers.py
+++ api/ellandi/registration/serializers.py
@@ -188,9 +188,9 @@ class LearningListSerializer(serializers.ListSerializer):
 class BaseLearningSerializer(serializers.Serializer):
     id = serializers.UUIDField(format="hex_verbose", required=False)
     name = serializers.CharField(max_length=255, required=False)
-    duration_minutes = serializers.IntegerField(max_value=32767, min_value=0, required=False)
-    date_completed = serializers.DateField(required=False)
-    cost_pounds = serializers.IntegerField(max_value=32767, min_value=0, required=False)
+    duration_minutes = serializers.IntegerField(max_value=32767, min_value=0, required=False, allow_null=True)
+    date_completed = serializers.DateField(required=False, allow_null=True)
+    cost_pounds = serializers.IntegerField(max_value=32767, min_value=0, required=False, allow_null=True)
     cost_unknown = serializers.BooleanField(required=False)
 
     class Meta:
