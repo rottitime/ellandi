dce26864aefb1b986a2bfe87a5d8c65575a91324
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-11-09 13:31:52 +0000

    Prefetch: learning type

--- web/service/api.ts
+++ web/service/api.ts
@@ -10,35 +10,6 @@ export const fetchSkills = async (): Promise<string[]> => {
   throw new Error(defaultError)
 }
 
-export const fetchLearningTypes = async (): Promise<GenericDataList[]> => {
-  // const res = await fetch(`${publicRuntimeConfig.apiUrl}/learning-types/`)
-  // if (res.ok) return res.json()
-  // throw new Error(defaultError)
-  return Promise.resolve([
-    {
-      slug: 'string',
-      name: 'On the job',
-      description:
-        'Self-taught learning by doing, for example reading policies and guidance, using tools and software to do your job',
-      order: 0
-    },
-    {
-      slug: 'string',
-      name: 'Social',
-      description:
-        'Learning from colleagues, job shadowing, mentoring, coaching, networks and communities',
-      order: 1
-    },
-    {
-      slug: 'string',
-      name: 'Formal',
-      description:
-        'Completing a course on Civil Service Learning, external training, professional qualifications',
-      order: 2
-    }
-  ])
-}
-
 export const fetchFeedback = async (
   data: FeedabckType
 ): Promise<{ success: boolean }> => {
