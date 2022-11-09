8640d053ce84314c31a282e242c0109fda7fb4fc
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-11-09 18:02:20 +0000

    Storybok data optout

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -14,7 +14,8 @@ module.exports = {
   ],
   framework: '@storybook/react',
   core: {
-    builder: '@storybook/builder-webpack5'
+    builder: '@storybook/builder-webpack5',
+    disableTelemetry: true
   },
   typescript: {
     check: true
