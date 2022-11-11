90dc0121786eeaf9b908e3322601f8da3ec797cb
Ed Singleton <singletoned@gmail.com>
2022-11-11 10:53:55 +0000

    Tidy

--- api/ellandi/registration/management/commands/make_admin.py
+++ api/ellandi/registration/management/commands/make_admin.py
@@ -7,11 +7,11 @@ class Command(BaseCommand):
     help = "Make users admins"
 
     def add_arguments(self, parser):
-        parser.add_argument('emails', nargs='+')
+        parser.add_argument("emails", nargs="+")
 
     def handle(self, *args, **kwargs):
-        for email in kwargs['emails']:
+        for email in kwargs["emails"]:
             user = models.User.objects.get(email=email)
             user.is_staff = True
             user.save()
-            print(f"Made {email} an admin")
+            print(f"Made {email} an admin")  # noqa T201
