3384daca1859d0f98948bfd8e2c69006f5c46f89
Ed Singleton <singletoned@gmail.com>
2022-11-11 10:43:30 +0000

    Make users an admin

---
+++ api/ellandi/registration/management/commands/make_admin.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+
+from ellandi.registration import models
+
+
+class Command(BaseCommand):
+    help = "Make users admins"
+
+    def add_arguments(self, parser):
+        parser.add_argument('emails', nargs='+')
+
+    def handle(self, *args, **kwargs):
+        for email in kwargs['emails']:
+            user = models.User.objects.get(email=email)
+            user.is_staff = True
+            user.save()
+            print(f"Made {email} an admin")
