2b0c5c1c266d2454c364be41c936e4c443afb7f0
Nina Menezes <nina.menezes@example.com>
2022-11-14 16:46:46 +0000

    fix migrations

--- api/ellandi/registration/migrations/0034_loaddata_fixtures_business_units.py
+++ api/ellandi/registration/migrations/0034_loaddata_fixtures_business_units.py
@@ -2,9 +2,9 @@ from django.core.management import call_command
 from django.db import migrations
 
 
-def reload_professions(apps, schema_editor):
-    Profession = apps.get_model("registration", "BusinessUnit")
-    Profession.objects.all().delete()
+def reload_business_units(apps, schema_editor):
+    BusinessUnit = apps.get_model("registration", "BusinessUnit")
+    BusinessUnit.objects.all().delete()
     fixture_file = "dropdown/businessunits.json.json"
     call_command("loaddata", fixture_file, app_label="registration", ignorenonexistent=True)
 
@@ -15,4 +15,4 @@ class Migration(migrations.Migration):
         ("registration", "0033_auto_20221113_2014"),
     ]
 
-    operations = [migrations.RunPython(reload_professions)]
+    operations = [migrations.RunPython(reload_business_units)]
