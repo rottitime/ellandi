e73234402b2e0e77323c55628257bbafc65abae0
Ed Singleton <singletoned@gmail.com>
2022-11-15 21:51:41 +0000

    Add arguments to script

--- scripts/add_courses.py
+++ scripts/add_courses.py
@@ -1,7 +1,7 @@
+import argparse
 import getpass
 import json
 import pathlib
-import sys
 
 import httpx
 
@@ -10,18 +10,17 @@ __here__ = pathlib.Path(__file__).parent
 courses_file = __here__ / ".." / "courses-summary.json"
 
 
-def get_token_and_url(token=None):
-    base_url  = input("Base url of the site (default http://localhost)") or "http://localhost"
+def get_token_and_url(token=None, base_url=None):
+    if not base_url:
+        base_url  = input("Base url of the site (default http://localhost)") or "http://localhost"
 
     if not token:
         email = input("Admin email: ")
         password = getpass.getpass("Password: ")
-    with httpx.Client(base_url=base_url) as client:
-        response = client.post("/api/login/", json={'email': email, 'password': password})
-        print(response)
-        print(response.json())
-        token = response.json()['token']
-
+        with httpx.Client(base_url=base_url, timeout=30) as client:
+            response = client.post("/api/login/", json={'email': email, 'password': password})
+            token = response.json()['token']
+            print(f"Got token: {token}")
 
     data = {
         'token': token,
@@ -30,25 +29,34 @@ def get_token_and_url(token=None):
     return data
 
 
-def load_courses(base_url, token):
-    with courses_file.open() as f:
-        data = json.load(f)
+def load_courses(input_file, base_url, token):
+    data = json.load(input_file)
 
     with httpx.Client(base_url="http://localhost", timeout=30) as client:
         headers = {"Authorization": f"Token {token}"}
         client.headers = headers
 
+        current_courses = client.get("/api/courses/").json()
+        print(current_courses)
+        titles = set(course['title'] for course in current_courses)
+
         for course in data:
-            client.patch("/api/add-course/", json=course)
+            if course['title'] in titles:
+                print(f"Not adding {course['title']}")
+            else:
+                client.patch("/api/add-course/", json=course)
+                print(f"Added {course['title']}")
 
 
 def main():
-    if len(sys.argv) > 1:
-        token = sys.argv[1]
-    else:
-        token = None
-    data = get_token_and_url(token)
-    load_courses(**data)
+    parser = argparse.ArgumentParser(prog = 'Add courses', description = 'Add some courses to a remote server')
+    parser.add_argument('--token', default="", help='A login token')
+    parser.add_argument('--base_url', default="", help='The base url of the remote server')
+    parser.add_argument('--file', type=argparse.FileType('r'), help="The json file of courses", required=True)
+    args = parser.parse_args()
+
+    data = get_token_and_url(token=args.token, base_url=args.base_url)
+    load_courses(input_file=args.file, **data)
 
 
 if __name__ == '__main__':
