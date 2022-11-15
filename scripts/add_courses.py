246a176510b92fdc537cd28dcca6dff475999aa1
Ed Singleton <singletoned@gmail.com>
2022-11-15 22:00:33 +0000

    Tidy

--- scripts/add_courses.py
+++ scripts/add_courses.py
@@ -5,27 +5,23 @@ import pathlib
 
 import httpx
 
-
 __here__ = pathlib.Path(__file__).parent
 courses_file = __here__ / ".." / "courses-summary.json"
 
 
 def get_token_and_url(token=None, base_url=None):
     if not base_url:
-        base_url  = input("Base url of the site (default http://localhost)") or "http://localhost"
+        base_url = input("Base url of the site (default http://localhost)") or "http://localhost"
 
     if not token:
         email = input("Admin email: ")
         password = getpass.getpass("Password: ")
         with httpx.Client(base_url=base_url, timeout=30) as client:
-            response = client.post("/api/login/", json={'email': email, 'password': password})
-            token = response.json()['token']
-            print(f"Got token: {token}")
+            response = client.post("/api/login/", json={"email": email, "password": password})
+            token = response.json()["token"]
+            print(f"Got token: {token}")  # noqa T201
 
-    data = {
-        'token': token,
-        'base_url': base_url
-    }
+    data = {"token": token, "base_url": base_url}
     return data
 
 
@@ -37,27 +33,26 @@ def load_courses(input_file, base_url, token):
         client.headers = headers
 
         current_courses = client.get("/api/courses/").json()
-        print(current_courses)
-        titles = set(course['title'] for course in current_courses)
+        titles = set(course["title"] for course in current_courses)
 
         for course in data:
-            if course['title'] in titles:
-                print(f"Not adding {course['title']}")
+            if course["title"] in titles:
+                print(f"Not adding {course['title']}")  # noqa T201
             else:
                 client.patch("/api/add-course/", json=course)
-                print(f"Added {course['title']}")
+                print(f"Added {course['title']}")  # noqa T201
 
 
 def main():
-    parser = argparse.ArgumentParser(prog = 'Add courses', description = 'Add some courses to a remote server')
-    parser.add_argument('--token', default="", help='A login token')
-    parser.add_argument('--base_url', default="", help='The base url of the remote server')
-    parser.add_argument('--file', type=argparse.FileType('r'), help="The json file of courses", required=True)
+    parser = argparse.ArgumentParser(prog="Add courses", description="Add some courses to a remote server")
+    parser.add_argument("--token", default="", help="A login token")
+    parser.add_argument("--base_url", default="", help="The base url of the remote server")
+    parser.add_argument("--file", type=argparse.FileType("r"), help="The json file of courses", required=True)
     args = parser.parse_args()
 
     data = get_token_and_url(token=args.token, base_url=args.base_url)
     load_courses(input_file=args.file, **data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
