305b8b3e07277c63b9ac8352f69ea1bbbe578c60
Nina Menezes <nina.menezes@example.com>
2022-11-29 14:29:20 +0000

    blacken

--- api/ellandi/registration/fakers.py
+++ api/ellandi/registration/fakers.py
@@ -10,7 +10,7 @@ fake = faker.Faker()
 
 def make_fake_course():
     data = {
-        "id": fake.unique.pystr_format("?"*22, letters=string.ascii_letters+string.digits+"_-"),
+        "id": fake.unique.pystr_format("?" * 22, letters=string.ascii_letters + string.digits + "_-"),
         "title": fake.sentence(),
         "short_description": fake.paragraph(),
         "long_description": "\n".join(fake.paragraphs()),
