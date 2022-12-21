994551da3ea4757096f9fbfe2ae6846dfea4b3aa
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-12-21 14:49:48 +0000

    Lint issue fix

--- web/__tests__/account/invite.test.tsx
+++ web/__tests__/account/invite.test.tsx
@@ -1,8 +1,8 @@
 import { screen, waitFor, within } from '@testing-library/react'
 import InvitePage from '@/pages/account/invite'
 import fetchMock from 'jest-fetch-mock'
-import { renderWithProviders, mockMe } from '@/lib/test-utils'
-import { InvitedMembers, InviteMember, RegisterUserResponse } from '@/service/types'
+import { renderWithProviders } from '@/lib/test-utils'
+import { InvitedMembers } from '@/service/types'
 import userEvent from '@testing-library/user-event'
 
 const mockInviteList: InvitedMembers[] = [
