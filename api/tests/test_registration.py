fb61e3d7baf14e3edfcfb2dd94ebd2facb1893d1
Ed Singleton <singletoned@gmail.com>
2022-12-21 15:57:36 +0000

    Add test for invites

--- api/tests/test_registration.py
+++ api/tests/test_registration.py
@@ -735,3 +735,25 @@ def test_has_reporting_access_non_admin(client, user_id):
     assert response.status_code == status.HTTP_200_OK
     data = response.json()
     assert not data["has_reports_access"]
+
+
+@utils.with_logged_in_client
+def test_invite_friend(client, user_id):
+    names = ("Bobby Bobbington", "Freddy Freddington", "Geoffry Geoffrington")
+    data = tuple(
+        {"email": "{}@example.com".format(name.replace(" ", ".")), "first_name": name.split()[0]} for name in names
+    )
+    for i, datum in enumerate(data):
+        response = client.patch("/api/me/invites/", json=datum)
+        assert response.status_code == status.HTTP_200_OK
+
+        expected_data = list(
+            dict(status="Pending", email=item["email"].lower(), first_name=item["first_name"]) for item in data[: i + 1]
+        )
+        assert response.json() == expected_data
+
+    expected_data = list(
+        dict(status="Pending", email=item["email"].lower(), first_name=item["first_name"]) for item in data
+    )
+    response = client.get("/api/me/invites/")
+    assert response.json() == expected_data
